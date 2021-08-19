fb13197c6c73071b7823e7bf9db9c8c953b97174
Rilabeast <ricardoholmes2004@gmail.com>
2021-08-20 00:29:49 +0200

    Prepared files for day 21

---
+++ Day 21/Day21.cs
@@ -0,0 +1,11 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Day20
+{
+    public static void Main(string[] args)
+    {
+        List<string> puzzleInput = File.ReadLines("input.txt").ToList();
+    }
+}
