04d8de240156fb0fe10de58eafc43063e13bd55a
Rilabeast <ricardoholmes2004@gmail.com>
2021-08-21 19:40:14 +0200

    Created files for day 22

---
+++ Day 22/Day22.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Day22
+{
+    public static void Main(string[] args)
+    {
+    }
+}
