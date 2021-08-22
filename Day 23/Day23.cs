bd2b5ac009f536dd36fa957f51841222b9fd6c47
Rilabeast <ricardoholmes2004@gmail.com>
2021-08-22 13:27:56 +0200

    Created files for day 23

---
+++ Day 23/Day23.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Day23
+{
+    public static void Main(string[] args)
+    {
+        string input = "459672813";
+    }
+}
