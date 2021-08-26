32ea8ff415aa65a544472869e5ce4df9a0cfccea
Rilabeast <ricardoholmes2004@gmail.com>
2021-08-26 20:13:46 +0200

    Created files for day 24

---
+++ Day 24/Day24.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+class Day24
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
