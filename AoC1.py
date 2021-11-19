59af31dbaadf92cc5ef39f41928986c88743fd2c
Rilabeast <ricardoholmes2004@gmail.com>
2021-11-19 11:35:03 +0100

    Removed old python solution to day 1 part 1

--- AoC1.py
+++
@@ -1,9 +0,0 @@
-with open("input1.txt") as file:
-    f = (file.read()).split("\n")
-    total = 0
-    for i in f:
-        tempFuel = int(i)
-        while ((tempFuel//3)-2) > 0:
-            tempFuel = ((tempFuel//3)-2)
-            total += tempFuel
-print(total)
