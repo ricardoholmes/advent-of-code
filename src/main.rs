151c70ac336bb87a6540b1efd1a0b124c873611a
Rilabeast <ricardoholmes2004@gmail.com>
2021-11-29 11:25:35 +0100

    Added support for puzzles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
+mod modules;
+
 fn main() {
-    println!("Hello, world!");
+    let mut question_number = String::new();
+    println!("Enter the question number: ");
+
+    io::stdin()
+        .read_line(&mut question_number)
+        .expect("Failed to read line");
+        
+    println!("");
+
+    let question_number: u16 = question_number
+        .trim()
+        .parse()
+        .expect("Please enter a number");
+
+    match question_number {
+        1 => modules::day_one(),
+        _ => println!("Invalid question number"),
+    }
 }
---
+++ src/modules/mod.rs
@@ -0,0 +1,5 @@
+mod day_one;
+
+pub fn day_one() {
+    day_one::run();
+}
