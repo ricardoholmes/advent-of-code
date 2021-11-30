4f88fc1ee840e16b8ccd81bf74f18cf3b31fefc8
Rilabeast <ricardoholmes2004@gmail.com>
2021-11-30 10:37:13 +0100

    Can now run a specific day from terminal with command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 use std::io;
+use std::env;
 
 mod modules;
 
 fn main() {
+    // get sysargs for day num
+    let sysargs: Vec<String> = env::args().collect();
+
     let mut day_num = String::new();
-    println!("Enter the question number:");
 
-    io::stdin()
-        .read_line(&mut day_num)
-        .expect("Failed to read line");
-        
-    println!("");
+    // if no sysarg then:
+    if sysargs.len() < 2 {
+        println!("Enter the question number:");
 
+        io::stdin()
+            .read_line(&mut day_num)
+            .expect("Failed to read line");
+            
+        println!("");
+
+    }
+    
+    else {
+        day_num = (*sysargs[1]).to_string();
+    }
+    
     let day_num: u16 = day_num
         .trim()
         .parse()
         .expect("Please enter a number");
-
+    
     modules::run(day_num);
 }
