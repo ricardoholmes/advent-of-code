6e9fb24a8b2d666747df8085651cf192ea281bef
Rilabeast <ricardoholmes2004@gmail.com>
2021-12-02 10:25:52 +0100

    Day 2 complete

--- src/solutions/day_2.rs
+++ src/solutions/day_2.rs
@@ -4,14 +4,15 @@ pub fn run() {
     let input_str = fs::read_to_string("inputs/input_2.txt")
         .expect("Failed to read file");
 
+    part1((*input_str).to_string());
+    part2((*input_str).to_string());
+}
+
+fn part1(input_str: String) {
     let input: Vec<&str> = input_str
         .split("\n")
         .collect();
-    
-        part1(input)
-}
 
-fn part1(input: Vec<&str>) {
     let mut horizontal_pos = 0;
     let mut vertical_pos = 0;
     for command in input {
@@ -28,6 +29,38 @@ fn part1(input: Vec<&str>) {
             "forward" => horizontal_pos += magnitude,
             "up" => vertical_pos -= magnitude,
             "down" => vertical_pos += magnitude,
+            _ => println!("Error with direction")
+        }
+    }
+
+    println!("{}", (horizontal_pos * vertical_pos))
+}
+
+fn part2(input_str: String) {
+    let input: Vec<&str> = input_str
+        .split("\n")
+        .collect();
+
+    let mut aim = 0;
+    let mut horizontal_pos = 0;
+    let mut vertical_pos = 0;
+    for command in input {
+        let command_split: Vec<&str> = command
+            .split(" ")
+            .collect();
+
+        let direction = command_split[0];
+        let magnitude: i32 = (*command_split[1])
+            .parse()
+            .expect("Error parsing magnitude");
+
+        match direction {
+            "forward" => {
+                horizontal_pos += magnitude;
+                vertical_pos += aim * magnitude;
+            },
+            "up" => aim -= magnitude,
+            "down" => aim += magnitude,
             _ => println!("Error with direction")
         }
     }
