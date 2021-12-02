315049ca643010905c441f8f66c253ae3304346b
Rilabeast <ricardoholmes2004@gmail.com>
2021-12-02 10:40:48 +0100

    Cleaned up my day 1 solution

--- src/solutions/day_1.rs
+++ src/solutions/day_1.rs
@@ -9,8 +9,14 @@ pub fn run() {
         .map(|s| s.parse().expect("parse error"))
         .collect();
     
+    part1(&input);
+    part2(&input);
+}
+
+fn part1(input: &Vec<i32>) {
     let mut previous = input[0];
     let mut increasing_count = 0;
+
     for i in &input[1..] {
         if *i > previous {
             increasing_count += 1;
@@ -19,15 +25,19 @@ pub fn run() {
         previous = *i;
     }
     println!("Part 1: {}", increasing_count);
+}
 
-    previous = input[..3].iter().sum();
-    increasing_count = 0;
+fn part2(input: &Vec<i32>) {
+    let mut previous = input[0] + input[1] + input[2];
+    let mut increasing_count = 0;
+
     for i in 1..(input.len()-2) {
-        let sum = &input[i..(i+3)].iter().sum();
-        if *sum > previous {
+        let sum = input[i] + input[i+1] + input[i+2];
+        if sum > previous {
             increasing_count += 1;
         }
-        previous = *sum;
+
+        previous = sum;
     }
     println!("Part 2: {}", increasing_count);
 }
