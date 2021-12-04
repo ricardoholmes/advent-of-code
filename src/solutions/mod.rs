ac2759552555aeb832caeb4a18d9da12fff50c59
Rilabeast <ricardoholmes2004@gmail.com>
2021-12-04 12:18:54 +0100

    DAY 4 PART 1 DONE

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,12 +1,14 @@
 mod day_1;
 mod day_2;
 mod day_3;
+mod day_4;
 
 pub fn run(day: u16) {
     match day {
         1 => day_1::run(),
         2 => day_2::run(),
         3 => day_3::run(),
+        4 => day_4::run(),
         _ => println!("Invalid day number"),
     }
 }
