7ebb1c2652d865f206ea9f0ce47224b5a617b005
Rilabeast <ricardoholmes2004@gmail.com>
2021-12-05 13:31:37 +0100

    DAY 5 COMPLETE

--- src/solutions/day_5.rs
+++ src/solutions/day_5.rs
@@ -28,12 +28,15 @@ pub fn run() {
         lines.push([[start_point[0], start_point[1]], [end_point[0], end_point[1]]]);
     }
 
-    part_one(&lines);
+    let (remaining_lines, points_crossed, count) = part_one(&lines);
+    part_two(&remaining_lines, points_crossed, count);
 }
 
-fn part_one(lines: &Vec<[[i32; 2]; 2]>) {
-    let mut count = 0;
+fn part_one(lines: &Vec<[[i32; 2]; 2]>) -> (Vec<[[i32; 2]; 2]>, HashMap<[i32; 2], i32>, i32) {
+    let mut remaining_lines: Vec<[[i32; 2]; 2]> = vec!();
     let mut points_crossed: HashMap<[i32; 2], i32> = HashMap::new();
+    let mut count: i32 = 0;
+
     for line in lines {
         // if the line is vertical
         if line[0][0] == line[1][0] {
@@ -96,7 +99,49 @@ fn part_one(lines: &Vec<[[i32; 2]; 2]>) {
                 }
             }
         }
+
+        else {
+            remaining_lines.push(*line);
+        }
     }
 
     println!("Part 1: {}", count);
+
+    // to avoid too much code repetition
+    (remaining_lines, points_crossed, count)
+}
+
+fn part_two(lines: &Vec<[[i32; 2]; 2]>, mut points_crossed: HashMap<[i32; 2], i32>, mut count: i32) {
+    for line in lines {
+        // will only be diagonal lines so no need to use ifs
+        let start_point = line[0];
+        let mut end_point = line[1];
+
+        let x_change = if end_point[0] > start_point[0] { 1 } else { -1 };
+        let y_change = if end_point[1] > start_point[1] { 1 } else { -1 };
+
+        // to account for last point
+        end_point[0] += x_change;
+        end_point[1] += y_change;
+
+        let mut point = start_point;
+        while point != end_point {
+            if points_crossed.contains_key(&point) {
+                if points_crossed[&point] == 1 {
+                    count += 1
+                }
+
+                *points_crossed.get_mut(&point).unwrap() += 1;
+            }
+
+            else {
+                points_crossed.insert(point, 1);
+            }
+
+            point[0] += x_change;
+            point[1] += y_change;
+        }
+    }
+
+    println!("Part 2: {}", count);
 }
