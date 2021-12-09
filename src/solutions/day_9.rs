88e6b48b33055df8f254c9c1cd5e2dbd578e94c9
Rilabeast <ricardoholmes2004@gmail.com>
2021-12-09 20:19:15 +0100

    Day 9 done

--- src/solutions/day_9.rs
+++ src/solutions/day_9.rs
@@ -19,24 +19,79 @@ pub fn run() {
     }
 
     part_one(&height_map);
+    part_two(&height_map);
 }
 
 fn part_one(height_map: &Vec<Vec<i32>>) {
     let mut total_risk_level = 0;
     for y in 0..height_map.len() {
         for x in 0..height_map[0].len() {
-            let height = height_map[y][x];
-
-            let mut is_lowest = x == 0 || height < height_map[y][x-1];
-            is_lowest = is_lowest && (x == height_map[0].len() - 1 || height < height_map[y][x+1]);
-            is_lowest = is_lowest && (y == 0 || height < height_map[y-1][x]);
-            is_lowest = is_lowest && (y == height_map.len() - 1 || height < height_map[y+1][x]);
-
-            if is_lowest {
-                total_risk_level += 1 + height;
+            if is_lowest(height_map, x, y) {
+                total_risk_level += 1 + height_map[y][x];
             }
         }
     }
 
     println!("Part 1: {}", total_risk_level);
+}
+
+fn part_two(height_map: &Vec<Vec<i32>>) {
+    let mut basins: Vec<i32> = vec!();
+    for y in 0..height_map.len() {
+        for x in 0..height_map[0].len() {
+            if is_lowest(height_map, x, y) {
+                basins.push(basin_size(height_map, x, y, vec!()).0);
+            }
+        }
+    }
+
+    basins.sort();
+    let mut sizes_mult = 1;
+    for i in 1..4 {
+        sizes_mult *= basins[basins.len()-i];
+    }
+    println!("Part 2: {}", sizes_mult);
+}
+
+fn is_lowest(height_map: &Vec<Vec<i32>>, x: usize, y: usize) -> bool {
+    let height = height_map[y][x];
+
+    let mut lowest = x == 0 || height < height_map[y][x-1];
+    lowest = lowest && (x == height_map[0].len() - 1 || height < height_map[y][x+1]);
+    lowest = lowest && (y == 0 || height < height_map[y-1][x]);
+    lowest = lowest && (y == height_map.len() - 1 || height < height_map[y+1][x]);
+
+    lowest
+}
+
+fn basin_size(height_map: &Vec<Vec<i32>>, x: usize, y: usize, mut visited_coords: Vec<(usize, usize)>) -> (i32, Vec<(usize, usize)>) {
+    let height = height_map[y][x];
+    visited_coords.push((x, y));
+    let mut total_size = 1;
+
+    if x != 0 && !visited_coords.contains(&(x-1, y)) && height < height_map[y][x-1] && height_map[y][x-1] != 9 {
+        let (size, coords) = basin_size(height_map, x-1, y, visited_coords);
+        visited_coords = coords;
+        total_size += size;
+    }
+
+    if x != height_map[0].len() - 1 && !visited_coords.contains(&(x+1, y)) && height < height_map[y][x+1] && height_map[y][x+1] != 9 {
+        let (size, coords) = basin_size(height_map, x+1, y, visited_coords);
+        visited_coords = coords;
+        total_size += size;
+    }
+
+    if y != 0 && !visited_coords.contains(&(x, y-1)) && height < height_map[y-1][x] && height_map[y-1][x] != 9 {
+        let (size, coords) = basin_size(height_map, x, y-1, visited_coords);
+        visited_coords = coords;
+        total_size += size;
+    }
+
+    if y != height_map.len() - 1 && !visited_coords.contains(&(x, y+1)) && height < height_map[y+1][x] && height_map[y+1][x] != 9 {
+        let (size, coords) = basin_size(height_map, x, y+1, visited_coords);
+        visited_coords = coords;
+        total_size += size;
+    }
+
+    (total_size, visited_coords)
 }
