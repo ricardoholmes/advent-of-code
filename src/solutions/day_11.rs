e3d9daacd5a9251e69596c01cdd8bce3aa25a5ab
Rilabeast <ricardoholmes2004@gmail.com>
2021-12-11 13:55:05 +0100

    Day 11 done!

--- src/solutions/day_11.rs
+++ src/solutions/day_11.rs
@@ -11,6 +11,7 @@ pub fn run() {
         .collect();
 
     part_one(input.clone());
+    part_two(input.clone());
 }
 
 fn part_one(mut grid: Vec<Vec<u32>>) {
@@ -24,12 +25,12 @@ fn part_one(mut grid: Vec<Vec<u32>>) {
             }
         }
 
-        // vector of octopuses/octopi/octopodes (coords) that have already flashed
+        // vector of octopi/octopodes/octopuses (coords) that have already flashed
         let mut flashed: Vec<(usize, usize)> = vec!();
         loop {
             // find all coordinates that are 0
             let mut zeros: Vec<(usize, usize)> = get_zeros(&grid);
-            // filter out octopodes that have already flashed
+            // filter out octopi that have already flashed
             zeros.retain(|e| !flashed.contains(e));
 
             // break from loop if no octopus is at 0
@@ -64,6 +65,64 @@ fn part_one(mut grid: Vec<Vec<u32>>) {
     println!("Part 1: {}", total_flashes);
 }
 
+fn part_two(mut grid: Vec<Vec<u32>>) {
+    let mut turn = 0;
+    loop {
+        turn += 1;
+
+        // grid is always 10x10 so no need to use grid.len()
+        for y in 0..10 {
+            for x in 0..10 {
+                // increase octopus charge by 1, but if go back to 0 if charge becomes 10
+                grid[y][x] = (grid[y][x] + 1) % 10;
+            }
+        }
+
+        // vector of octopi (coords) that have already flashed
+        let mut flashed: Vec<(usize, usize)> = vec!();
+        loop {
+            // find all coordinates that are 0
+            let mut zeros: Vec<(usize, usize)> = get_zeros(&grid);
+            // filter out octopi that have already flashed
+            zeros.retain(|e| !flashed.contains(e));
+
+            // break from loop if no octopus is at 0
+            if zeros.len() == 0 {
+                break;
+            }
+
+            for (x, y) in zeros {
+                flashed.push((x, y));
+                
+                // calculate bounds of loop (to prevent overflow)
+                let lower_x_bound = if x == 0 { 0 } else { x - 1 };
+                let upper_x_bound = if x == 9 { 10 } else { x + 2 };
+
+                let lower_y_bound = if y == 0 { 0 } else { y - 1 };
+                let upper_y_bound = if y == 9 { 10 } else { y + 2 };
+
+                // iterate through all adjacent cells
+                for y1 in lower_y_bound..upper_y_bound {
+                    for x1 in lower_x_bound..upper_x_bound {
+                        // if cell isn't at 0, increase charge by 1
+                        if grid[y1][x1] != 0 {
+                            grid[y1][x1] = (grid[y1][x1] + 1) % 10;
+                        }
+                    }
+                }
+            }
+        }
+
+        // if all octopus have flashed this turn, break
+        if flashed.len() == 100 {
+            break;
+        }
+    }
+
+    println!("Part 2: {}", turn);
+}
+
+// get all coordinates of cells that have 0 charge
 fn get_zeros(grid: &Vec<Vec<u32>>) -> Vec<(usize, usize)> {
     let mut zero_coords: Vec<(usize, usize)> = vec!();
     for y in 0..10 {
