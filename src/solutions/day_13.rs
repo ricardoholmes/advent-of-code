a4b12d2e39680338915ede569c0bff5bf68ef059
Rilabeast <ricardoholmes2004@gmail.com>
2021-12-13 12:03:29 +0100

    Day 13 done

--- src/solutions/day_13.rs
+++ src/solutions/day_13.rs
@@ -11,10 +11,14 @@ pub fn run() {
         .map(|s| s.split("\n").collect::<Vec<&str>>())
         .collect();
 
-    let grid: Vec<Vec<i32>> = input[0]
-        .iter()
-        .map(|s| s.split(",").map(|x| x.parse().unwrap()).collect::<Vec<i32>>())
-        .collect();
+    let mut grid: HashSet<(i32, i32)> = HashSet::new();
+    for line in &input[0] {
+        let line_split: Vec<&str> = line
+            .split(",")
+            .collect();
+
+        grid.insert((line_split[0].parse().unwrap(), line_split[1].parse().unwrap()));
+    }
 
     let mut folds: Vec<(&str, i32)> = vec!();
     for line in &input[1] {
@@ -25,32 +29,69 @@ pub fn run() {
         folds.push((line_split[0], line_split[1].parse().unwrap()));
     }
 
-    part_one(&grid, folds[0]);
+    part_one(&grid, &folds);
+    part_two(grid, &folds);
 }
 
-fn part_one(grid: &Vec<Vec<i32>>, fold: (&str, i32)) {
+fn part_one(grid: &HashSet<(i32, i32)>, folds: &Vec<(&str, i32)>) {
+    println!("Part 1: {}", fold_grid(grid, folds[0]).len());
+}
+
+fn part_two(mut grid: HashSet<(i32, i32)>, folds: &Vec<(&str, i32)>) {
+    for fold in folds {
+        grid = fold_grid(&grid, *fold);
+    }
+
+    let mut grid_width = 0;
+    let mut grid_height = 0;
+    for i in &grid {
+        if i.0 + 1 > grid_width {
+            grid_width = i.0 + 1;
+        }
+
+        if i.1 + 1> grid_height {
+            grid_height = i.1 + 1;
+        }
+    }
+
+    println!("Part 2:");
+    for i in 0..grid_height {
+        for j in 0..grid_width {
+            if grid.contains(&(j, i)) {
+                print!("#");
+            }
+            else {
+                print!(" ");
+            }
+        }
+        print!("\n");
+    }
+    print!("\n");
+}
+
+fn fold_grid(grid: &HashSet<(i32, i32)>, fold: (&str, i32)) -> HashSet<(i32, i32)> {
     let mut cells: HashSet<(i32, i32)> = HashSet::new();
     if fold.0 == "x" {
         for cell in grid {
-            if cell[0] > fold.1 {
-                cells.insert((fold.1 - (cell[0] - fold.1), cell[1]));
+            if cell.0 > fold.1 {
+                cells.insert((fold.1 - (cell.0 - fold.1), cell.1));
             }
             else {
-                cells.insert((cell[0], cell[1]));
+                cells.insert((cell.0, cell.1));
             }
         }
     }
 
     else {
         for cell in grid {
-            if cell[1] > fold.1 {
-                cells.insert((cell[0], fold.1 - (cell[1] - fold.1)));
+            if cell.1 > fold.1 {
+                cells.insert((cell.0, fold.1 - (cell.1 - fold.1)));
             }
             else {
-                cells.insert((cell[0], cell[1]));
+                cells.insert((cell.0, cell.1));
             }
         }
     }
 
-    println!("Part 1: {}", cells.len());
+    cells
 }
