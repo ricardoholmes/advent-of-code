b0f4639e7f499d82d9b867c48da581a17163ccff
Ricardo Holmes <ricardoholmes2004@gmail.com>
2022-12-01 05:31:14 +0000

    Cleaned up code significantly

--- src/solutions/day_1.rs
+++ src/solutions/day_1.rs
@@ -1,77 +1,33 @@
 pub fn run() {
     let input_str = include_str!("../../inputs/input_1.txt");
 
-    let input: Vec<Vec<u32>> = input_str
+    // make input into list of sums of calories
+    let mut input: Vec<u32> = input_str
         .replace("\r\n", "\n")
         .split("\n\n")
-        .map(|s| s.split("\n").map(|x| x.parse().unwrap()).collect::<Vec<u32>>())
+        .map(|s| s.split("\n")
+            .map(|x| x.parse().unwrap())
+            .collect::<Vec<u32>>()
+            .iter()
+            .sum()
+        )
         .collect();
+
+    // sort in decending order
+    input.sort();
+    input.reverse();
 
     part_one(&input);
     part_two(&input);
 }
 
-fn part_one(input: &Vec<Vec<u32>>) {
-    let mut sum;
-    let mut highest =  0;
-    for i in input {
-        sum = 0;
-        for j in i {
-            sum += j;
-        }
-        if sum > highest {
-            highest = sum;
-        }
-    }
-
-    println!("Part one: {highest}");
+fn part_one(input: &Vec<u32>) {
+    // get the biggest sum
+    println!("Part one: {}", input[0]);
 }
 
-fn part_two(input: &Vec<Vec<u32>>) {
-    let mut sum;
-    let mut highest =  0;
-    for i in input {
-        sum = 0;
-        for j in i {
-            sum += j;
-        }
-        if sum > highest {
-            highest = sum;
-        }
-    }
-
-    let mut second_highest =  0;
-    for i in input {
-        sum = 0;
-        for j in i {
-            sum += j;
-        }
-
-        if sum == highest {
-            continue;
-        }
-
-        if sum > second_highest {
-            second_highest = sum;
-        }
-    }
-
-    let mut third_highest =  0;
-    for i in input {
-        sum = 0;
-        for j in i {
-            sum += j;
-        }
-
-        if sum == highest || sum == second_highest {
-            continue;
-        }
-
-        if sum > third_highest {
-            third_highest = sum;
-        }
-    }
-
-    let total = highest + second_highest + third_highest;
+fn part_two(input: &Vec<u32>) {
+    // add the top 3 biggest sums
+    let total: u32 = input.iter().take(3).sum();
     println!("Part two: {total}");
 }
