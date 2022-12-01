94ce06a70b95738c20e52c19586abf44c4988d6f
Ricardo Holmes <ricardoholmes2004@gmail.com>
2022-12-01 05:08:34 +0000

    Completed day 1 part 1

--- src/solutions/day_1.rs
+++ src/solutions/day_1.rs
@@ -1,3 +1,27 @@
 pub fn run() {
-    println!("Hello, world!")
+    let input_str = include_str!("../../inputs/input_1.txt");
+
+    let input: Vec<Vec<u32>> = input_str
+        .replace("\r\n", "\n")
+        .split("\n\n")
+        .map(|s| s.split("\n").map(|x| x.parse().unwrap()).collect::<Vec<u32>>())
+        .collect();
+
+    part_one(&input);
+}
+
+fn part_one(input: &Vec<Vec<u32>>) {
+    let mut sum;
+    let mut highest =  0;
+    for i in input {
+        sum = 0;
+        for j in i {
+            sum += j;
+        }
+        if sum > highest {
+            highest = sum;
+        }
+    }
+
+    println!("Part one: {highest}");
 }
