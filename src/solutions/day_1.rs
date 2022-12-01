6a45e023535c7e17a2d8a4eae7bb6f4d2784a67a
Ricardo Holmes <ricardoholmes2004@gmail.com>
2022-12-01 05:17:07 +0000

    Completed day 1 part 2, very dumb solution but it works

--- src/solutions/day_1.rs
+++ src/solutions/day_1.rs
@@ -8,6 +8,7 @@ pub fn run() {
         .collect();
 
     part_one(&input);
+    part_two(&input);
 }
 
 fn part_one(input: &Vec<Vec<u32>>) {
@@ -24,4 +25,53 @@ fn part_one(input: &Vec<Vec<u32>>) {
     }
 
     println!("Part one: {highest}");
+}
+
+fn part_two(input: &Vec<Vec<u32>>) {
+    let mut sum;
+    let mut highest =  0;
+    for i in input {
+        sum = 0;
+        for j in i {
+            sum += j;
+        }
+        if sum > highest {
+            highest = sum;
+        }
+    }
+
+    let mut second_highest =  0;
+    for i in input {
+        sum = 0;
+        for j in i {
+            sum += j;
+        }
+
+        if sum == highest {
+            continue;
+        }
+
+        if sum > second_highest {
+            second_highest = sum;
+        }
+    }
+
+    let mut third_highest =  0;
+    for i in input {
+        sum = 0;
+        for j in i {
+            sum += j;
+        }
+
+        if sum == highest || sum == second_highest {
+            continue;
+        }
+
+        if sum > third_highest {
+            third_highest = sum;
+        }
+    }
+
+    let total = highest + second_highest + third_highest;
+    println!("Part two: {total}");
 }
