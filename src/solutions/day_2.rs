453592363a1c81032a2a45fa54609d4a78febf1f
Ricardo Holmes <ricardoholmes2004@gmail.com>
2022-12-02 05:33:23 +0000

    Completed day 2 part 2

--- src/solutions/day_2.rs
+++ src/solutions/day_2.rs
@@ -1,25 +1,22 @@
 pub fn run() {
     let input_str = include_str!("../../inputs/input_2.txt");
 
-    let input: Vec<&str> = input_str
+    let input: Vec<Vec<&str>> = input_str
         .strip_suffix("\n")
         .unwrap()
         .split("\n")
-        .collect();
-
-    part_one(&input);
-}
-
-fn part_one(input: &Vec<&str>) {
-    let games: Vec<Vec<&str>> = input
-        .iter()
         .map(|i| i
             .split(" ")
             .collect::<Vec<&str>>()
         ).collect();
 
+    part_one(&input);
+    part_two(&input);
+}
+
+fn part_one(input: &Vec<Vec<&str>>) {
     let mut score = 0;
-    for game in games {
+    for game in input {
         score += match game[0] {
             "A" => if game[1] == "Y" { 6 } else { if game[1] == "X" { 3 } else { 0 } },
             "B" => if game[1] == "Z" { 6 } else { if game[1] == "Y" { 3 } else { 0 } },
@@ -41,5 +38,22 @@ fn part_one(input: &Vec<&str>) {
         }
     }
 
-    println!("Part one: {score}")
+    println!("Part one: {score}");
+}
+
+fn part_two(input: &Vec<Vec<&str>>) {
+    let mut score = 0;
+    for game in input {
+        score += match game[1] {
+            "X" => 0 + if game[0] == "A" { 3 } else { if game[0] == "C" { 2 } else { 1 }},
+            "Y" => 3 + if game[0] == "C" { 3 } else { if game[0] == "B" { 2 } else { 1 }},
+            "Z" => 6 + if game[0] == "B" { 3 } else { if game[0] == "A" { 2 } else { 1 }},
+            _ => {
+                println!("Invalid character '{}'", game[0]);
+                0
+            },
+        };
+    }
+
+    println!("Part two: {score}");
 }
