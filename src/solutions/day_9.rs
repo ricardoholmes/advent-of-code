50bb8d7ead0a0c9ec3f0a15dd34f53d48e566434
Ricardo Holmes <ricardoholmes2004@gmail.com>
2022-12-09 08:01:16 +0000

    Day 9 part 2 done

--- src/solutions/day_9.rs
+++ src/solutions/day_9.rs
@@ -14,6 +14,7 @@ pub fn run() {
         }).collect();
 
     part_one(&input);
+    part_two(&input);
 }
 
 fn part_one(input: &Vec<(&str, u32)>) {
@@ -62,4 +63,54 @@ fn part_one(input: &Vec<(&str, u32)>) {
     }
 
     println!("Part one: {}", coords_visited.len());
+}
+
+fn part_two(input: &Vec<(&str, u32)>) {
+    let mut coords_visited: HashSet<(i32, i32)> = HashSet::new();
+    coords_visited.insert((0, 0));
+
+    let mut sections: [(i32, i32); 10] = [(0, 0); 10];
+
+    for &(direction, magnitude) in input {
+        let x_change = match direction {
+            "L" => -1,
+            "R" => 1,
+            _ => 0
+        };
+        let y_change = match direction {
+            "D" => -1,
+            "U" => 1,
+            _ => 0
+        };
+
+        for _ in 0..magnitude {
+            sections[0].0 += x_change;
+            sections[0].1 += y_change;
+
+            for i in 1..=9 {
+                let x_diff: i32 = sections[i-1].0 - sections[i].0;
+                let y_diff: i32 = sections[i-1].1 - sections[i].1;
+
+                if x_diff.abs() <= 1 && y_diff.abs() <= 1 {
+                    continue;
+                }
+                else if x_diff == 0 {
+                    sections[i].1 += if y_diff > 0 { 1 } else { -1 };
+                }
+                else if y_diff == 0 {
+                    sections[i].0 += if x_diff > 0 { 1 } else { -1 };
+                }
+                else {
+                    sections[i].0 += if x_diff > 0 { 1 } else { -1 };
+                    sections[i].1 += if y_diff > 0 { 1 } else { -1 };
+                }
+
+                // println!("{direction} => {head_coords:?} {tail_coords:?}");
+            }
+
+            coords_visited.insert(sections[9]);
+        }
+    }
+
+    println!("Part two: {}", coords_visited.len());
 }
