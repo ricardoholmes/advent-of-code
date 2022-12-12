14a457152c8b745b1c91ba4e4f08eddcc44ab6e4
Ricardo Holmes <ricardoholmes2004@gmail.com>
2022-12-12 08:48:19 +0000

    Day 12 Part 2 done

--- src/solutions/day_12.rs
+++ src/solutions/day_12.rs
@@ -11,6 +11,7 @@ pub fn run() {
         .collect();
 
     part_one(&input);
+    part_two(&input);
 }
 
 fn part_one(input: &Vec<Vec<char>>) {
@@ -76,4 +77,69 @@ fn part_one(input: &Vec<Vec<char>>) {
     }
 
     println!("Part one: {}", paths.pop_back().unwrap().len() - 1);
+}
+
+fn part_two(input: &Vec<Vec<char>>) {
+    let mut visited_nodes: HashSet<(usize, usize)> = HashSet::new();
+    let mut paths: VecDeque<Vec<(usize, usize)>> = VecDeque::new();
+
+    let mut start_node: (usize, usize) = (0, 0);
+    'outer: for row in 0..input.len() {
+        for column in 0..input[row].len() {
+            if input[row][column] == 'E' {
+                start_node = (row, column);
+                break 'outer;
+            }
+        }
+    }
+    visited_nodes.insert(start_node);
+
+    for (row_offset, column_offset) in vec!((0, 1), (2, 1), (1, 0), (1, 2)) {
+        if start_node.0 + row_offset == 0 || start_node.0 + row_offset-1 >= input.len()
+            || start_node.1 + column_offset == 0 || start_node.1 + column_offset-1 >= input[0].len()
+        {
+            continue;
+        }
+
+        let coord = (start_node.0 + row_offset-1, start_node.1 + column_offset-1);
+        let cell = input[start_node.0 + row_offset-1][start_node.1 + column_offset-1];
+
+        if cell == 'z' || cell == 'y' {
+            visited_nodes.insert(coord);
+            paths.push_back(vec!(start_node, coord));
+        }
+    }
+
+    'outer: loop {
+        let path = paths[0].clone();
+        let current_node = path.last().unwrap();
+        let current_node_val = input[current_node.0][current_node.1];
+
+        for (row_offset, column_offset) in vec!((0, 1), (2, 1), (1, 0), (1, 2)) {
+            if current_node.0 + row_offset == 0 || current_node.0 + row_offset-1 >= input.len() ||
+                current_node.1 + column_offset == 0 || current_node.1 + column_offset-1 >= input[0].len()
+            {
+                continue;
+            }
+
+            let coord = (current_node.0 + row_offset-1, current_node.1 + column_offset-1);
+            let cell = input[current_node.0 + row_offset-1][current_node.1 + column_offset-1];
+
+            if !visited_nodes.contains(&coord) && (cell == current_node_val ||
+                cell as u32 >= current_node_val as u32 - 1 ||
+                (current_node_val == 'b' && cell == 'S'))
+            {
+                visited_nodes.insert(coord);
+                let mut new_path = path.clone();
+                new_path.push(coord);
+                paths.push_back(new_path);
+                if cell == 'a' || cell == 'S' {
+                    break 'outer;
+                }
+            }
+        }
+        paths.pop_front();
+    }
+
+    println!("Part two: {}", paths.pop_back().unwrap().len() - 1);
 }
