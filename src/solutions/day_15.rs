fbc7ad6d95c5dbe5f7f6a03fbd616a854442abb9
Ricardo Holmes <ricardoholmes2004@gmail.com>
2022-12-15 12:54:17 +0000

    Optimised part 1 significantly

--- src/solutions/day_15.rs
+++ src/solutions/day_15.rs
@@ -39,7 +39,8 @@ pub fn run() {
 
 fn part_one(sensors: &Vec<Sensor>) {
     const ROW: i64 = 2_000_000;
-    let mut cannot_contain: HashSet<(i64, i64)> = HashSet::new();
+    let mut cannot_contain_ranges: Vec<i64> = vec![];
+    let mut x_beacons_on_row: HashSet<i64> = HashSet::new();
 
     for sensor in sensors {
         let distance = sensor.beacon_rel_pos.0.abs() + sensor.beacon_rel_pos.1.abs();
@@ -48,14 +49,30 @@ fn part_one(sensors: &Vec<Sensor>) {
         }
         let max_x_offset: i64 = if sensor.pos.1 < ROW { sensor.pos.1 + distance - ROW } else { ROW - (sensor.pos.1 - distance) };
 
-        for x_offset in -max_x_offset..=max_x_offset {
-            if sensor.beacon_rel_pos != (x_offset, ROW - sensor.pos.1) {
-                cannot_contain.insert((sensor.pos.0 + x_offset, ROW));
+        if sensor.beacon_rel_pos.1 == ROW {
+            x_beacons_on_row.insert(sensor.beacon_rel_pos.0);
+        }
+
+        let min_x = sensor.pos.0 - max_x_offset;
+        let max_x = sensor.pos.0 + max_x_offset;
+        insert_range(&mut cannot_contain_ranges, &(min_x, max_x));
+    }
+
+    let mut ranges: Vec<i64> = vec![];
+    for range in cannot_contain_ranges.chunks(2) {
+        insert_range(&mut ranges, &(range[0], range[1]));
+    }
+
+    let mut count = 0;
+    for range in ranges.chunks(2) {
+        count += range[1] - range[0];
+        for &beacon in &x_beacons_on_row {
+            if beacon >= range[0] && beacon <= range[1] {
+                count -= 1;
             }
         }
     }
-
-    println!("Part one: {}", cannot_contain.len());
+    println!("Part one: {}", count);
 }
 
 fn part_two(sensors: &Vec<Sensor>) {
@@ -82,5 +99,41 @@ fn part_two(sensors: &Vec<Sensor>) {
                 return;
             }
         }
+    }
+}
+
+fn insert_range(ranges: &mut Vec<i64>, range_to_insert: &(i64, i64)) {
+    let mut inserted = false;
+    let min = range_to_insert.0;
+    let max = range_to_insert.1;
+    for i in 0..ranges.len()/2 {
+        let range = &mut ranges[i*2..=(i*2)+1];
+        if min >= range[0] && min <= range[1] && max > range[1] {
+            range[1] = max;
+            inserted = true;
+            break;
+        }
+        else if min < range[0] && max >= range[0] && max <= range[1] {
+            range[0] = min;
+            inserted = true;
+            break;
+        }
+        else if min < range[0] && max > range[1] {
+            range[0] = min;
+            range[1] = max;
+            inserted = true;
+            break;
+        }
+        else if min >= range[0] && max <= range[1] {
+            inserted = true;
+            break;
+        }
+        else {
+        }
+    }
+    
+    if !inserted {
+        ranges.push(min);
+        ranges.push(max);
     }
 }
