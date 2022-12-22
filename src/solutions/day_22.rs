b5706c0b57ba0f1736ac0c000efcd362c90b115a
Ricardo Holmes <ricardoholmes2004@gmail.com>
2022-12-22 21:45:09 +0100

    Completed day 22 part 2

--- src/solutions/day_22.rs
+++ src/solutions/day_22.rs
@@ -1,8 +1,34 @@
-use std::collections::HashSet;
-
+#[derive(Debug)]
 enum Movement {
     Direction(char),
     Magnitude(i32),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Facing {
+    Right,
+    Down,
+    Left,
+    Up,
+}
+
+impl Facing {
+    fn turn_left(&mut self) {
+        const DIRECTIONS: [Facing; 4] = [Facing::Right, Facing::Down, Facing::Left, Facing::Up];
+        let directions_index = (DIRECTIONS.iter().position(|i| i == self).unwrap() as i32 - 1).rem_euclid(4) as usize;
+        *self = DIRECTIONS[directions_index];
+    }
+
+    fn turn_right(&mut self) {
+        const DIRECTIONS: [Facing; 4] = [Facing::Right, Facing::Down, Facing::Left, Facing::Up];
+        let directions_index = (DIRECTIONS.iter().position(|i| i == self).unwrap() as i32 + 1).rem_euclid(4) as usize;
+        *self = DIRECTIONS[directions_index];
+    }
+
+    fn value(&self) -> usize {
+        const DIRECTIONS: [Facing; 4] = [Facing::Right, Facing::Down, Facing::Left, Facing::Up];
+        DIRECTIONS.iter().position(|i| i == self).unwrap()
+    }
 }
 
 pub fn run() {
@@ -37,48 +63,46 @@ pub fn run() {
         num.clear();
     }
 
-    part_one(&map, &path)
+    part_one(&map, &path);
+    part_two(&map, &path);
 }
 
 fn part_one(map: &Vec<Vec<char>>, path: &Vec<Movement>) {
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
     let (mut x, mut y) = (map[0].iter().position(|&c| c == '.').unwrap(), 0);
-    visited.insert((x, y));
 
-    let mut facing: i32 = 0;
+    let mut facing = Facing::Right;
     for step in path {
         match step {
-            Movement::Direction(dir) => facing = match dir {
-                'L' => (facing - 1).rem_euclid(4),
-                'R' => (facing + 1).rem_euclid(4),
+            Movement::Direction(dir) => match dir {
+                'L' => facing.turn_left(),
+                'R' => facing.turn_right(),
                 _ => panic!()
             },
             Movement::Magnitude(magnitude) => for _ in 0..*magnitude {
                 let mut next_x: i32 = x as i32;
                 let mut next_y: i32 = y as i32;
                 match facing {
-                    0 => next_x += 1,
-                    1 => next_y += 1,
-                    2 => next_x -= 1,
-                    3 => next_y -= 1,
-                    _ => panic!()
+                    Facing::Right => next_x += 1,
+                    Facing::Down => next_y += 1,
+                    Facing::Left => next_x -= 1,
+                    Facing::Up => next_y -= 1,
                 };
-                if next_x < 0 || (facing == 2 && map[y][next_x as usize] == ' ') {
+                if next_x < 0 || (facing == Facing::Left && map[y][next_x as usize] == ' ') {
                     while next_x < map[y].len() as i32 - 1 && map[y][(next_x + 1) as usize] != ' ' {
                         next_x += 1;
                     }
                 }
-                if next_x >= map[y].len() as i32 || (facing == 0 && map[y][next_x as usize] == ' ') {
+                else if next_x >= map[y].len() as i32 || (facing == Facing::Right && map[y][next_x as usize] == ' ') {
                     while next_x > 0 && map[y][(next_x - 1) as usize] != ' ' {
                         next_x -= 1;
                     }
                 }
-                if next_y < 0 || (facing == 3 && (map[next_y as usize].len() <= x || map[next_y as usize][x] == ' ')) {
+                else if next_y < 0 || (facing == Facing::Up && (map[next_y as usize].len() <= x || map[next_y as usize][x] == ' ')) {
                     while next_y < map.len() as i32 - 1 && map[(next_y + 1) as usize].len() > x && map[(next_y + 1) as usize][x] != ' ' {
                         next_y += 1;
                     }
                 }
-                if next_y >= map.len() as i32 || (facing == 1 && (map[next_y as usize].len() <= x || map[next_y as usize][x] == ' ')) {
+                else if next_y >= map.len() as i32 || (facing == Facing::Down && (map[next_y as usize].len() <= x || map[next_y as usize][x] == ' ')) {
                     while next_y > 0 && map[(next_y - 1) as usize].len() > x && map[(next_y - 1) as usize][x] != ' ' {
                         next_y -= 1;
                     }
@@ -91,10 +115,123 @@ fn part_one(map: &Vec<Vec<char>>, path: &Vec<Movement>) {
 
                 x = next_x as usize;
                 y = next_y as usize;
-                visited.insert((x, y));
             }
         };
     }
 
-    println!("Part one: {}", 1000*(y + 1) + 4*(x + 1) + facing as usize);
+    println!("Part one: {}", 1000*(y + 1) + 4*(x + 1) + facing.value());
+}
+
+fn part_two(map: &Vec<Vec<char>>, path: &Vec<Movement>) {
+    let (mut x, mut y) = (map[0].iter().position(|&c| c == '.').unwrap()+1, 1);
+
+    let mut facing = Facing::Right;
+    for step in path {
+        match step {
+            Movement::Direction(dir) => match dir {
+                'L' => facing.turn_left(),
+                'R' => facing.turn_right(),
+                _ => panic!()
+            },
+            Movement::Magnitude(magnitude) => for _ in 0..*magnitude {
+                let mut next_x = x;
+                let mut next_y = y;
+                let mut next_facing = facing;
+                match facing {
+                    Facing::Right => next_x += 1,
+                    Facing::Down => next_y += 1,
+                    Facing::Left => next_x -= 1,
+                    Facing::Up => next_y -= 1,
+                }
+
+                // hard coded wrapping because doing it properly is too hard
+
+                // wrap right
+                if next_x == 151 && (1..=50).contains(&y) {
+                    next_x = 100;
+                    next_y = 151 - y;
+                    next_facing = Facing::Left;
+                }
+                else if next_x == 101 && (51..=100).contains(&y) {
+                    next_x = y + 50;
+                    next_y = 50;
+                    next_facing = Facing::Up;
+                }
+                else if next_x == 101 && (101..=150).contains(&y){
+                    next_x = 150;
+                    next_y = 151 - y;
+                    next_facing = Facing::Left;
+                }
+                else if next_x == 51 && (151..=200).contains(&y) {
+                    next_x = y - 100;
+                    next_y = 150;
+                    next_facing = Facing::Up;
+                }
+
+                // wrap left
+                else if next_x == 50 && (1..=50).contains(&y) {
+                    next_x = 1;
+                    next_y = 151 - y;
+                    next_facing = Facing::Right;
+                }
+                else if next_x == 50 && (51..=100).contains(&y) {
+                    next_x = y - 50;
+                    next_y = 101;
+                    next_facing = Facing::Down;
+                }
+                else if next_x == 0 && (101..=150).contains(&y) {
+                    next_x = 51;
+                    next_y = 151 - y;
+                    next_facing = Facing::Right;
+                }
+                else if next_x == 0 && (151..=200).contains(&y) {
+                    next_x = y - 100;
+                    next_y = 1;
+                    next_facing = Facing::Down;
+                }
+
+                // wrap up
+                else if next_y == 100 && (1..=50).contains(&x) {
+                    next_x = 51;
+                    next_y = x + 50;
+                    next_facing = Facing::Right;
+                }
+                else if next_y == 0 && (51..=100).contains(&x) {
+                    next_x = 1;
+                    next_y = x + 100;
+                    next_facing = Facing::Right;
+                }
+                else if next_y == 0 && (101..=150).contains(&x) {
+                    next_x = x - 100;
+                    next_y = 200;
+                    next_facing = Facing::Up;
+                }
+                
+                // wrap down
+                else if next_y == 201 && (1..=50).contains(&x) {
+                    next_x = x + 100;
+                    next_y = 1;
+                    next_facing = Facing::Down;
+                }
+                else if next_y == 151 && (51..=100).contains(&x) {
+                    next_x = 50;
+                    next_y = x + 100;
+                    next_facing = Facing::Left;
+                }
+                else if next_y == 51 && (101..=150).contains(&x) {
+                    next_x = 100;
+                    next_y = x - 50;
+                    next_facing = Facing::Left;
+                }
+
+                if map[next_y - 1][next_x - 1] != '#' {
+                    x = next_x;
+                    y = next_y;
+                    facing = next_facing;
+                }
+            }
+        }
+    }
+
+    println!("Part two: {}", 1000*y + 4*x + facing.value());
 }
