bcda67a997c3e8ff91405530be8123b0c92b5e7d
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-01 00:25:33 +0000

    Make main completely safe

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -5,48 +5,98 @@ use std::io::Write;
 
 mod solutions;
 
-fn main() {
-    // get sysargs for day num
-    let sysargs: Vec<String> = env::args().collect();
+fn main() -> Result<(), String> {
+    match get_day_num() {
+        Ok(day_num) => {
+            match day_num {
+                Some(day) => run_day(day),
+                None => run_all(),
+            }
+        },
+        Err(e) => Err(e)
+    }
+}
 
-    let day_num: String = match sysargs.get(1) {
-        Some(x) => x.to_string(),
+fn run_day(day: u16) -> Result<(), String> {
+    println!("\n--- Day {day} ---");
+
+    let t = std::time::Instant::now();
+    match solutions::run(day) {
+        Ok(_) => (),
+        Err(e) => return Err(e),
+    };
+
+    println!("Time taken: {:?}", std::time::Instant::now() - t);
+    Ok(())
+}
+
+fn run_all() -> Result<(), String> {
+    let dir = match fs::read_dir("src/solutions") {
+        Ok(d) => d,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    let latest_solution = dir.filter(|file| 
+        {
+            let filename = match file {
+                    Ok(f) => f,
+                    Err(_) => return false,
+            }.file_name();
+
+            match filename.to_str() {
+                Some(filename) => filename.starts_with("day"),
+                None => false,
+            }
+        }
+    ).fold(0, |sum, _| sum + 1);
+
+    println!("======================================");
+    println!("  A D V E N T  O F  C O D E  2 0 2 3  ");
+    println!("======================================");
+    let start_time = std::time::Instant::now();
+    for day_num in 1..=latest_solution {
+        match run_day(day_num) {
+            Ok(_) => (),
+            Err(e) => return Err(e),
+        }
+    }
+    println!();
+
+    println!("=================");
+    println!(" F I N I S H E D ");
+    println!("=================");
+
+    println!("Done in {:?}", std::time::Instant::now() - start_time);
+    Ok(())
+}
+
+fn get_day_num() -> Result<Option<u16>, String> {
+    // check sys_args for day num
+    let sys_args: Vec<String> = env::args().collect();
+
+    let day_num: String = match sys_args.get(1) {
+        Some(x) => x.to_owned(),
         None => {
             let mut buffer = String::new();
             print!("\nEnter the question number: ");
-            io::stdout().flush().expect("Failed to flush stdout");
-            io::stdin().read_line(&mut buffer).expect("Failed to read line");
+            match io::stdout().flush() {
+                Ok(_) => (),
+                Err(e) => return Err(e.to_string()),
+            };
+            match io::stdin().read_line(&mut buffer) {
+                Ok(_) => (),
+                Err(e) => return Err(e.to_string()),
+            };
             buffer.trim().to_string()
         }
     };
 
-    if day_num == "all" {
-        let dir = fs::read_dir("./src/solutions").unwrap();
-        let latest_solution = dir.filter(|file| 
-            file.as_ref().unwrap().file_name().to_str().unwrap().starts_with("day")
-        ).fold(0, |sum, _| sum + 1);
-
-        println!("======================================");
-        println!("  A D V E N T  O F  C O D E  2 0 2 3  ");
-        println!("======================================");
-        let start_time = std::time::Instant::now();
-        for day_num in 1..=latest_solution {
-            if day_num == 16 || day_num == 18 {
-                // too slow to run for now
-                continue;
-            }
-            solutions::run(day_num);
-        }
-        println!("\n=================");
-        println!(" F I N I S H E D ");
-        println!("=================");
-        println!("Done in {:?}", std::time::Instant::now() - start_time);
-    }
-
-    else {
-        match day_num.parse() {
-            Ok(day) => solutions::run(day),
-            Err(_) => println!("\nERROR : Failed to parse day number"),
+    match day_num.parse() {
+        Ok(n) => Ok(Some(n)),
+        Err(e) => if day_num == "all" {
+            Ok(None)
+        } else {
+            Err(e.to_string())
         }
     }
 }
--- 2023/src/solutions/mod.rs
+++ 2023/src/solutions/mod.rs
@@ -1,13 +1,10 @@
 mod day_1;
 
-pub fn run(day: u16) {
-    println!("\n--- Day {day} ---");
-
-    let t = std::time::Instant::now();
+pub fn run(day: u16) -> Result<(), String> {
     match day {
         1 => day_1::run(),
-        _ => println!("Invalid day number"),
+        _ => return Err(String::from("Invalid day number")),
     };
 
-    println!("Time taken: {:?}", std::time::Instant::now() - t);
+    Ok(())
 }
