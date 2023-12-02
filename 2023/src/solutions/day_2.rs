d26a1363177a2723596c1d81a5b7eaaeab75a59d
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-02 12:45:40 +0000

    Replace tuple with struct

--- 2023/src/solutions/day_2.rs
+++ 2023/src/solutions/day_2.rs
@@ -1,24 +1,31 @@
-const RED: usize = 0;
-const GREEN: usize = 1;
-const BLUE: usize = 2;
+struct Game {
+    id: u32,
+    red: u32,
+    green: u32,
+    blue: u32
+}
 
 pub fn run() -> Result<(), String> {
-    let input_str: Vec<&str> = include_str!("../../inputs/input_2.txt")
+    let input: Vec<&str> = include_str!("../../inputs/input_2.txt")
         .lines()
         .collect();
 
     // each element is: (id, [max_red_count, max_green_count, max_blue_count])
-    let mut games: Vec<(u32, [u32; 3])> = vec![];
+    let mut games: Vec<Game> = vec![];
 
-    for line in &input_str {
+    for line in &input {
         let line_split: Vec<&str> = line.split(": ").collect();
-        let id: u32 = line_split[0].split_ascii_whitespace().last().unwrap().parse().unwrap();
-        let game = line_split[1].replace("; ", ", ");
-        let game_properties = game.split(", ");
+        
+        let mut game = Game {
+            id: line_split[0].split_ascii_whitespace().last().unwrap().parse().unwrap(),
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
 
-        let mut red_count = 0;
-        let mut green_count = 0;
-        let mut blue_count = 0;
+        let game_str = line_split[1].replace("; ", ", ");
+        let game_properties = game_str.split(", ");
+
         for property in game_properties {
             let split: Vec<&str> = property.split_ascii_whitespace().collect();
 
@@ -29,18 +36,18 @@ pub fn run() -> Result<(), String> {
 
             let color = split[1];
 
-            if color == "red" && count > red_count {
-                red_count = count;
+            if color == "red" && count > game.red {
+                game.red = count;
             }
-            if color == "green" && count > green_count {
-                green_count = count;
+            if color == "green" && count > game.green {
+                game.green = count;
             }
-            if color == "blue" && count > blue_count {
-                blue_count = count;
+            if color == "blue" && count > game.blue {
+                game.blue = count;
             }
         }
 
-        games.push((id, [red_count, green_count, blue_count]));
+        games.push(game);
     }
 
     match part_one(&games) {
@@ -56,26 +63,26 @@ pub fn run() -> Result<(), String> {
     Ok(())
 }
 
-fn part_one(games: &[(u32, [u32; 3])]) -> Result<u32, String> {
+fn part_one(games: &[Game]) -> Result<u32, String> {
     Ok(games
         .iter()
         .fold(0, |total, game|
-            total + if game.1[RED] > 12 || 
-                        game.1[GREEN] > 13 ||
-                        game.1[BLUE] > 14 {
+            total + if game.red > 12 || 
+                        game.green > 13 ||
+                        game.blue > 14 {
                 0
             } else {
-                game.0
+                game.id
             }
         )
     )
 }
 
-fn part_two(games: &[(u32, [u32; 3])]) -> Result<u32, String> {
+fn part_two(games: &[Game]) -> Result<u32, String> {
     Ok(games
         .iter()
         .map(|game|
-            game.1[RED] * game.1[GREEN] * game.1[BLUE]
+            game.red * game.green * game.blue
         ).sum()
     )
 }
