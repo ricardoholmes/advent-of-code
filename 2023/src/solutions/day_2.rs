be27cb11703ab73e291f2c7b394c2956d5ebfdbe
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-02 10:12:18 +0000

    Add day 2 part 2 solution

--- 2023/src/solutions/day_2.rs
+++ 2023/src/solutions/day_2.rs
@@ -46,5 +46,33 @@ fn part_one(lines: &[&str]) -> Result<u32, String> {
 }
 
 fn part_two(lines: &[&str]) -> Result<u32, String> {
-    Ok(0)
+    let mut power: u32 = 0;
+    for &i in lines {
+        let line_split: Vec<&str> = i.split(": ").collect();
+        let game = line_split[1].replace("; ", ", ");
+        let game_properties = game.split(", ");
+
+        let mut red_count = 0;
+        let mut blue_count = 0;
+        let mut green_count = 0;
+        for property in game_properties {
+            let split: Vec<&str> = property.split_ascii_whitespace().collect();
+            
+            let count: u32 = split[0].parse().unwrap();
+            let color = split[1];
+
+            if color == "red" && count > red_count {
+                red_count = count;
+            }
+            if color == "blue" && count > blue_count {
+                blue_count = count;
+            }
+            if color == "green" && count > green_count {
+                green_count = count;
+            }
+        }
+
+        power += red_count * blue_count * green_count;
+    }
+    Ok(power)
 }
