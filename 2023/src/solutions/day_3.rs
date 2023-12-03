62eb77115fc691f6d37ce1bb991ab7e5102e0eed
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-03 13:50:15 +0000

    Add day 3 part 2 solution

--- 2023/src/solutions/day_3.rs
+++ 2023/src/solutions/day_3.rs
@@ -71,10 +71,82 @@ fn part_one(input: &Vec<Vec<char>>) -> Result<u32, String> {
 }
 
 fn part_two(input: &Vec<Vec<char>>) -> Result<u32, String> {
-    Ok(0)
+    let mut num_coords: HashSet<(usize, usize)> = HashSet::new();
+    let mut symbol_coords: HashSet<(usize, usize)> = HashSet::new();
+
+    let mut total: u32 = 0;
+    for line_index in 0..input.len() {
+        let line = input.get(line_index).unwrap();
+        for char_index in 0..line.len() {
+            let c = line.get(char_index).unwrap().to_owned();
+
+            if c != '*' {
+                continue;
+            }
+
+            let mut num_count: u32 = 0;
+            let mut num_coords_adjacent: HashSet<(usize, usize)> = HashSet::new();
+
+            let mut mult: u32 = 1;
+
+            let lines_range = (line_index.max(1)-1)..=(line_index+1).min(input.len()-1);
+            'outer: for y in lines_range {
+                let adj_line = input.get(y).unwrap();
+                let chars_range = (char_index.max(1)-1)..=(char_index+1).min(adj_line.len()-1);
+                for x in chars_range {
+                    if num_coords_adjacent.contains(&(y, x)) {
+                        continue;
+                    }
+
+                    let adj_char = adj_line.get(x).unwrap().to_owned();
+                    if adj_char.is_numeric() && adj_char != '.' {
+                        num_count += 1;
+                        if num_count > 2 {
+                            break 'outer;
+                        }
+                        mult *= get_num(&mut num_coords_adjacent, adj_line, x, y);
+                    }
+                }
+            }
+
+            if num_count == 2 {
+                num_coords.extend(&num_coords_adjacent);
+                symbol_coords.insert((line_index, char_index));
+                total += mult;
+            }
+        }
+    }
+
+    Ok(total)
 }
 
-fn visualise(input: &Vec<Vec<char>>, num_coords: &HashSet<(usize, usize)>, adj_coords: &HashSet<(usize, usize)>) {
+fn get_num(num_coords: &mut HashSet<(usize, usize)>, line: &Vec<char>, x: usize, y: usize) -> u32 {
+    let mut num_str = String::new();
+    for char_index in (0..x).rev() {
+        let ch = line.get(char_index).unwrap().to_owned();
+        if ch.is_numeric() {
+            num_str.push(ch);
+            num_coords.insert((y, char_index));
+        } else {
+            break;
+        }
+    }
+    num_str = num_str.chars().rev().collect();
+
+    for char_index in x..line.len() {
+        let ch = line.get(char_index).unwrap().to_owned();
+        if ch.is_numeric() {
+            num_str.push(ch);
+            num_coords.insert((y, char_index));
+        } else {
+            break;
+        }
+    }
+
+    num_str.parse().unwrap()
+}
+
+fn visualise(input: &Vec<Vec<char>>, num_coords: &HashSet<(usize, usize)>, symbol_coords: &HashSet<(usize, usize)>) {
     for line_index in 0..input.len() {
         let line = input.get(line_index).unwrap();
         for char_index in 0..line.len() {
@@ -85,7 +157,7 @@ fn visualise(input: &Vec<Vec<char>>, num_coords: &HashSet<(usize, usize)>, adj_c
             else if c.is_numeric() {
                 print!("{}", c.to_string().red());
             }
-            else if adj_coords.contains(&(line_index, char_index)) {
+            else if symbol_coords.contains(&(line_index, char_index)) {
                 print!("{}", c.to_string().yellow());
             }
             else {
@@ -114,6 +186,13 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, 1);
+        let example: Vec<Vec<char>> = include_str!("../../examples/day3_1.txt")
+            .trim()
+            .lines()
+            .map(|line| line.chars().collect())
+            .collect();
+
+        let result = part_two(&example);
+        assert_eq!(result, Ok(467835));
     }
 }
