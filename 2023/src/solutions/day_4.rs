83aa457ef6d0244c094510aead25a93d6da08868
ricardoholmes <ricardoholmes2004@gmail.com>
2023-12-04 13:18:16 +0000

    Group day 4 part 1 and 2 calculations

--- 2023/src/solutions/day_4.rs
+++ 2023/src/solutions/day_4.rs
@@ -1,47 +1,29 @@
 use crate::safe_unpack;
 
 pub fn run(input_raw: &str) -> Result<(), String> {
-    let parsed = parse(input_raw)?;
+    let solutions = parse(input_raw)?;
 
-    let answer_part_one = part_one(&parsed)?;
+    let answer_part_one = part_one(&solutions)?;
     println!("Part one: {}", answer_part_one);
 
-    let answer_part_two = part_two(&parsed)?;
+    let answer_part_two = part_two(&solutions)?;
     println!("Part two: {}", answer_part_two);
 
     Ok(())
 }
 
-fn parse(input_raw: &str) -> Result<Vec<&str>, String> {
-    let lines = input_raw.lines().collect();
-    Ok(lines)
-}
+fn parse(input_raw: &str) -> Result<(u32, u32), String> {
+    let lines: Vec<&str> = input_raw.lines().collect();
 
-fn part_one(lines: &[&str]) -> Result<u32, String> {
+    // variable needed for part 1
     let mut total = 0;
-    for line in lines {
-        safe_unpack!(line.split(": "), _, game);
-        safe_unpack!(game.split(" | "), win, held);
-        let win: Vec<u32> = win.split_ascii_whitespace().map(|i| i.parse().unwrap()).collect();
-        let held: Vec<u32> = held.split_ascii_whitespace().map(|i| i.parse().unwrap()).collect();
-        let mut winning = 0;
-        for i in held {
-            if win.contains(&i) {
-                winning += 1;
-            }
-        }
-        if winning > 0 {
-            total += 1 << (winning - 1);
-        }
-    }
-    Ok(total)
-}
 
-fn part_two(lines: &[&str]) -> Result<u32, String> {
+    // variables needed for part 2
     let mut instances = vec![1; lines.len()];
-    let mut cards = 0;
+    let mut card_count = 0;
+
     for i in 0..lines.len() {
-        cards += instances[i];
+        card_count += instances[i];
         let line = lines[i];
         safe_unpack!(line.split(": "), _, game);
         safe_unpack!(game.split(" | "), win, held);
@@ -54,12 +36,21 @@ fn part_two(lines: &[&str]) -> Result<u32, String> {
             }
         }
         if winning > 0 {
+            total += 1 << (winning - 1);
             for j in (i+1).min(lines.len()-1)..(i+winning+1).min(lines.len()) {
                 instances[j] += instances[i];
             }
         }
     }
-    Ok(cards)
+    Ok((total, card_count))
+}
+
+fn part_one(solutions: &(u32, u32)) -> Result<u32, String> {
+    Ok(solutions.0)
+}
+
+fn part_two(solutions: &(u32, u32)) -> Result<u32, String> {
+    Ok(solutions.1)
 }
 
 #[cfg(test)]
