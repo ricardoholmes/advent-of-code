64a09f2df137ecc0a76d32bd390b1a2b808f7d80
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-06 13:02:31 +0000

    Add day 6 part 2 solution

--- 2023/src/solutions/day_6.rs
+++ 2023/src/solutions/day_6.rs
@@ -12,7 +12,7 @@ pub fn run(input_raw: &str) -> Result<(), String> {
     Ok(())
 }
 
-fn parse(input_raw: &str) -> Result<Vec<(u32, u32)>, String> {
+fn parse(input_raw: &str) -> Result<(Vec<(u32, u32)>, (u64, u64)), String> {
     let mut lines = input_raw.lines();
 
     let mut times_raw: Vec<char> = lines
@@ -22,11 +22,13 @@ fn parse(input_raw: &str) -> Result<Vec<(u32, u32)>, String> {
         .collect();
 
     let mut times: Vec<u32> = vec![];
+    let mut time_part2: String = String::new();
     loop {
         times_raw = times_raw.iter().skip_while(|c| c.is_numeric()).skip_while(|c| !c.is_numeric()).map(|c| c.to_owned()).collect();
         if times_raw.len() == 0 {
             break;
         }
+        time_part2 = format!("{time_part2}{}", times_raw.iter().take_while(|c| c.is_numeric()).map(|c| *c).collect::<String>());
         let time: u32 = times_raw.iter().take_while(|c| c.is_numeric()).collect::<String>().parse().unwrap();
         times.push(time);
     }
@@ -38,38 +40,56 @@ fn parse(input_raw: &str) -> Result<Vec<(u32, u32)>, String> {
         .collect();
 
     let mut distances: Vec<u32> = vec![];
+    let mut distance_part2: String = String::new();
     loop {
         distances_raw = distances_raw.iter().skip_while(|c| c.is_numeric()).skip_while(|c| !c.is_numeric()).map(|c| c.to_owned()).collect();
         if distances_raw.len() == 0 {
             break;
         }
+        distance_part2 = format!("{distance_part2}{}", distances_raw.iter().take_while(|c| c.is_numeric()).map(|c| *c).collect::<String>());
         let distance: u32 = distances_raw.iter().take_while(|c| c.is_numeric()).collect::<String>().parse().unwrap();
         distances.push(distance);
     }
 
-    Ok(zip(times, distances).collect())
+    Ok((zip(times, distances).collect(), (time_part2.parse().unwrap(), distance_part2.parse().unwrap())))
 }
 
-fn part_one(solutions: &[(u32, u32)]) -> Result<u32, String> {
+fn part_one(solutions: &(Vec<(u32, u32)>, (u64, u64))) -> Result<u32, String> {
     let mut total = 1;
-    for (time, distance) in solutions {
+    for (time, distance) in solutions.0.clone() {
         let mut min_time = None;
-        for i in 1..*time {
-            if i * (time - i) > *distance {
+        for i in 1..time {
+            if i * (time - i) > distance {
                 min_time = Some(i);
                 break;
             }
         }
         if let Some(min_time) = min_time {
-            let num_ways = (*time - min_time + 1) - min_time;
+            let num_ways = (time - min_time + 1) - min_time;
             total *= num_ways;
         }
     }
+
     Ok(total)
 }
 
-fn part_two(solutions: &[(u32, u32)]) -> Result<u32, String> {
-    Ok(0)
+fn part_two(solutions: &(Vec<(u32, u32)>, (u64, u64))) -> Result<u64, String> {
+    let (time, distance) = solutions.1;
+
+    let mut min_time = None;
+    for i in 1..time {
+        if i * (time - i) > distance {
+            min_time = Some(i);
+            break;
+        }
+    }
+
+    let mut num_ways = None;
+    if let Some(min_time) = min_time {
+        num_ways = Some((time - min_time + 1) - min_time);
+    }
+
+    num_ways.ok_or(String::from("No minimum time found"))
 }
 
 #[cfg(test)]
@@ -91,6 +111,6 @@ mod tests {
 
         let parsed = parse(&example).unwrap();
         let result = part_two(&parsed);
-        assert_eq!(result, Ok(0));
+        assert_eq!(result, Ok(71503));
     }
 }
