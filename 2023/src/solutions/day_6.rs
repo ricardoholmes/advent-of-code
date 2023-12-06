dfb82a630d00a434df8279aa49ee1d66df75904a
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-06 14:22:23 +0000

    Improve speed of solution with maths B)

--- 2023/src/solutions/day_6.rs
+++ 2023/src/solutions/day_6.rs
@@ -12,7 +12,7 @@ pub fn run(input_raw: &str) -> Result<(), String> {
     Ok(())
 }
 
-fn parse(input_raw: &str) -> Result<(Vec<(u32, u32)>, (u64, u64)), String> {
+fn parse(input_raw: &str) -> Result<(Vec<(u64, u64)>, (u64, u64)), String> {
     let mut lines = input_raw.lines();
 
     let mut times_raw: Vec<char> = lines
@@ -21,15 +21,16 @@ fn parse(input_raw: &str) -> Result<(Vec<(u32, u32)>, (u64, u64)), String> {
         .chars()
         .collect();
 
-    let mut times: Vec<u32> = vec![];
+    let mut times: Vec<u64> = vec![];
     let mut time_part2: String = String::new();
     loop {
         times_raw = times_raw.iter().skip_while(|c| c.is_numeric()).skip_while(|c| !c.is_numeric()).map(|c| c.to_owned()).collect();
         if times_raw.len() == 0 {
             break;
         }
-        time_part2 = format!("{time_part2}{}", times_raw.iter().take_while(|c| c.is_numeric()).map(|c| *c).collect::<String>());
-        let time: u32 = times_raw.iter().take_while(|c| c.is_numeric()).collect::<String>().parse().unwrap();
+        let time_string: String = times_raw.iter().take_while(|c| c.is_numeric()).map(|c| *c).collect();
+        time_part2 = format!("{time_part2}{}", time_string);
+        let time: u64 = time_string.parse().unwrap();
         times.push(time);
     }
 
@@ -39,57 +40,48 @@ fn parse(input_raw: &str) -> Result<(Vec<(u32, u32)>, (u64, u64)), String> {
         .chars()
         .collect();
 
-    let mut distances: Vec<u32> = vec![];
+    let mut distances: Vec<u64> = vec![];
     let mut distance_part2: String = String::new();
     loop {
         distances_raw = distances_raw.iter().skip_while(|c| c.is_numeric()).skip_while(|c| !c.is_numeric()).map(|c| c.to_owned()).collect();
         if distances_raw.len() == 0 {
             break;
         }
-        distance_part2 = format!("{distance_part2}{}", distances_raw.iter().take_while(|c| c.is_numeric()).map(|c| *c).collect::<String>());
-        let distance: u32 = distances_raw.iter().take_while(|c| c.is_numeric()).collect::<String>().parse().unwrap();
+        let distance_string: String = distances_raw.iter().take_while(|c| c.is_numeric()).map(|c| *c).collect();
+        distance_part2 = format!("{distance_part2}{}", distance_string);
+        let distance: u64 = distance_string.parse().unwrap();
         distances.push(distance);
     }
 
     Ok((zip(times, distances).collect(), (time_part2.parse().unwrap(), distance_part2.parse().unwrap())))
 }
 
-fn part_one(solutions: &(Vec<(u32, u32)>, (u64, u64))) -> Result<u32, String> {
+fn part_one(solutions: &(Vec<(u64, u64)>, (u64, u64))) -> Result<u64, String> {
     let mut total = 1;
     for (time, distance) in solutions.0.clone() {
-        let mut min_time = None;
-        for i in 1..time {
-            if i * (time - i) > distance {
-                min_time = Some(i);
-                break;
-            }
-        }
-        if let Some(min_time) = min_time {
-            let num_ways = (time - min_time + 1) - min_time;
-            total *= num_ways;
-        }
+        total *= get_num_ways(time, distance);
     }
 
     Ok(total)
 }
 
-fn part_two(solutions: &(Vec<(u32, u32)>, (u64, u64))) -> Result<u64, String> {
+fn part_two(solutions: &(Vec<(u64, u64)>, (u64, u64))) -> Result<u64, String> {
     let (time, distance) = solutions.1;
 
-    let mut min_time = None;
-    for i in 1..time {
-        if i * (time - i) > distance {
-            min_time = Some(i);
-            break;
-        }
+    Ok(get_num_ways(time, distance))
+}
+
+fn get_num_ways(time: u64, distance: u64) -> u64 {
+    let discriminant = ((time * time) - (4 * distance)) as f64;
+    if discriminant < 0.0 {
+        return 0;
     }
 
-    let mut num_ways = None;
-    if let Some(min_time) = min_time {
-        num_ways = Some((time - min_time + 1) - min_time);
-    }
+    let time_for_distance = (time as f64 - discriminant.sqrt()) / 2.0;
+    let min_time = (time_for_distance + 1.0) as u64;
+    let num_ways = time + 1 - (2 * min_time);
 
-    num_ways.ok_or(String::from("No minimum time found"))
+    num_ways
 }
 
 #[cfg(test)]
