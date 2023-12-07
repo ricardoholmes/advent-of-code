0ab748495d4acd13a1ebdc8684e121b31203539c
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-07 18:59:04 +0000

    Add day 7 part 2 solution

--- 2023/src/solutions/day_7.rs
+++ 2023/src/solutions/day_7.rs
@@ -45,17 +45,18 @@ fn part_one(input: &[(&str, u32)]) -> Result<u32, String> {
     for (hand, bid) in input {
         let hand_type = get_hand_type(hand);
         let hand_values: Vec<u32> = hand.chars().map(|c| if c.is_ascii_digit() {
-            c.to_digit(10).unwrap()
-        } else {
-            match c {
-                'T' => 10,
-                'J' => 11,
-                'Q' => 12,
-                'K' => 13,
-                'A' => 14,
-                _ => panic!(),
-            }
-        }).collect();
+                c.to_digit(10).unwrap()
+            } else {
+                match c {
+                    'T' => 10,
+                    'J' => 11,
+                    'Q' => 12,
+                    'K' => 13,
+                    'A' => 14,
+                    _ => panic!(),
+                }
+            })
+            .collect();
 
         scored.push((hand_type, hand_values, *bid));
     }
@@ -73,8 +74,39 @@ fn part_one(input: &[(&str, u32)]) -> Result<u32, String> {
     Ok(total_winnings)
 }
 
-fn part_two(_input: &[(&str, u32)]) -> Result<u32, String> {
-    Ok(0)
+fn part_two(input: &[(&str, u32)]) -> Result<u32, String> {
+    let mut scored: Vec<(HandType, Vec<u32>, u32)> = vec![];
+
+    for (hand, bid) in input {
+        let hand_type = get_hand_type_with_joker(hand);
+        let hand_values: Vec<u32> = hand.chars().map(|c| if c.is_ascii_digit() {
+                c.to_digit(10).unwrap()
+            } else {
+                match c {
+                    'J' => 1,
+                    'T' => 10,
+                    'Q' => 11,
+                    'K' => 12,
+                    'A' => 13,
+                    _ => panic!(),
+                }
+            })
+            .collect();
+
+        scored.push((hand_type, hand_values, *bid));
+    }
+
+    scored.sort_unstable_by(|a, b| match a.0.partial_cmp(&b.0).unwrap() {
+        Ordering::Equal => a.1.partial_cmp(&b.1).unwrap(),
+        ord => ord,
+    });
+
+    let total_winnings = scored
+        .iter()
+        .enumerate()
+        .fold(0, |total, (index, hand)| total + hand.2 * (1 + index as u32));
+
+    Ok(total_winnings)
 }
 
 fn get_hand_type(hand: &str) -> HandType {
@@ -120,6 +152,23 @@ fn get_hand_type(hand: &str) -> HandType {
     hand_type
 }
 
+fn get_hand_type_with_joker(hand: &str) -> HandType {
+    let mut duplicates: Vec<(char, u32)>  = hand
+        .chars()
+        .fold(HashMap::new(), |mut map, c| {
+            if c != 'J' {
+                *map.entry(c).or_insert(0) += 1;
+            }
+            map
+        })
+        .into_iter()
+        .collect();
+
+    duplicates.sort_unstable_by_key(|i| i.1);
+    let better_hand = hand.replace('J', duplicates.last().unwrap_or(&('A', 0)).0.to_string().as_str());
+    println!("{hand} -> {better_hand}");
+    get_hand_type(&better_hand)
+}
 
 #[cfg(test)]
 mod tests {
@@ -140,6 +189,6 @@ mod tests {
 
         let parsed = parse(&example).unwrap();
         let result = part_two(&parsed);
-        assert_eq!(result, Ok(0));
+        assert_eq!(result, Ok(5905));
     }
 }
