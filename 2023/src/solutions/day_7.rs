f7bcb9e7376e28c7f7fd57e9c6bd35c2cfb5d179
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-07 18:35:36 +0000

    Add day 7 part 1 solution

---
+++ 2023/src/solutions/day_7.rs
@@ -0,0 +1,145 @@
+use std::{cmp::Ordering, collections::HashMap};
+
+#[derive(Debug, PartialEq, PartialOrd)]
+enum HandType {
+    HighCard,
+    OnePair,
+    TwoPairs,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+pub fn run(input_raw: &str) -> Result<(), String> {
+    let solutions = parse(input_raw)?;
+
+    let answer_part_one = part_one(&solutions)?;
+    println!("Part one: {}", answer_part_one);
+
+    let answer_part_two = part_two(&solutions)?;
+    println!("Part two: {}", answer_part_two);
+
+    Ok(())
+}
+
+fn parse(input_raw: &str) -> Result<Vec<(&str, u32)>, String> {
+    let lines_parsed: Vec<(&str, u32)> = input_raw
+        .lines()
+        .map(|line| {
+            let mut line_split = line.split_ascii_whitespace();
+            
+            (
+                line_split.next().unwrap(),
+                line_split.next().unwrap().parse().unwrap(),
+            )
+        })
+        .collect();
+
+    Ok(lines_parsed)
+}
+
+fn part_one(input: &[(&str, u32)]) -> Result<u32, String> {
+    let mut scored: Vec<(HandType, Vec<u32>, u32)> = vec![];
+
+    for (hand, bid) in input {
+        let hand_type = get_hand_type(hand);
+        let hand_values: Vec<u32> = hand.chars().map(|c| if c.is_ascii_digit() {
+            c.to_digit(10).unwrap()
+        } else {
+            match c {
+                'T' => 10,
+                'J' => 11,
+                'Q' => 12,
+                'K' => 13,
+                'A' => 14,
+                _ => panic!(),
+            }
+        }).collect();
+
+        scored.push((hand_type, hand_values, *bid));
+    }
+
+    scored.sort_unstable_by(|a, b| match a.0.partial_cmp(&b.0).unwrap() {
+        Ordering::Equal => a.1.partial_cmp(&b.1).unwrap(),
+        ord => ord,
+    });
+
+    let total_winnings = scored
+        .iter()
+        .enumerate()
+        .fold(0, |total, (index, hand)| total + hand.2 * (1 + index as u32));
+
+    Ok(total_winnings)
+}
+
+fn part_two(_input: &[(&str, u32)]) -> Result<u32, String> {
+    Ok(0)
+}
+
+fn get_hand_type(hand: &str) -> HandType {
+    let mut duplicates: Vec<u32>  = hand
+        .chars()
+        .fold(HashMap::new(), |mut map, c| {
+            *map.entry(c).or_insert(0) += 1;
+            map
+        })
+        .into_iter()
+        .map(|(_, v)| v)
+        .collect();
+
+    duplicates.sort_unstable();
+    duplicates.reverse();
+
+    let mut hand_type = HandType::HighCard;
+    for i in duplicates {
+        if i == 5 {
+            hand_type = HandType::FiveOfAKind;
+            break;
+        }
+        else if i == 4 {
+            hand_type = HandType::FourOfAKind;
+            break;
+        }
+        else if i == 3 {
+            hand_type = HandType::ThreeOfAKind;
+        }
+        else if i == 2 {
+            if hand_type == HandType::ThreeOfAKind {
+                hand_type = HandType::FullHouse;
+                break;
+            }
+            else if hand_type == HandType::OnePair {
+                hand_type = HandType::TwoPairs;
+                break;
+            }
+            hand_type = HandType::OnePair;
+        }
+    }
+
+    hand_type
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let example = include_str!("../../examples/day_7_1.txt");
+
+        let parsed = parse(&example).unwrap();
+        let result = part_one(&parsed);
+        assert_eq!(result, Ok(6440));
+    }
+
+    #[test]
+    fn test_part2() {
+        let example = include_str!("../../examples/day_7_1.txt");
+
+        let parsed = parse(&example).unwrap();
+        let result = part_two(&parsed);
+        assert_eq!(result, Ok(0));
+    }
+}
