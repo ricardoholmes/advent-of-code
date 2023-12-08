fddc4085d1006ce23fb9b8768014049e7e5d6d44
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-08 13:44:47 +0000

    Almost completed day 8 part 2 I think

--- 2023/src/solutions/day_8.rs
+++ 2023/src/solutions/day_8.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub fn parse(input_raw: &str) -> Result<(Vec<char>, HashMap<&str, (&str, &str)>), String> {
     let mut lines = input_raw.lines();
@@ -45,7 +45,66 @@ pub fn part_one(input: &(Vec<char>, HashMap<&str, (&str, &str)>)) -> Result<u64,
 }
 
 pub fn part_two(input: &(Vec<char>, HashMap<&str, (&str, &str)>)) -> Result<u64, String> {
+    let (instructions, network) = input.clone();
+    
+    let mut nodes: Vec<(&str, HashMap<(&str, usize), u64>)> = network
+        .keys()
+        .filter(|key| key.ends_with("A"))
+        .map(|node| (*node, HashMap::new()))
+        .collect();
+
+    let mut instruction_index;
+    for node in nodes.iter_mut() {
+        instruction_index = 0;
+        let mut steps = 0;
+
+        // (start_step_of_loop, length_of_loop, distance_to_ends)
+        let mut node_loop: (u64, u64, Vec<u64>) = (0, 0, vec![]);
+        loop {
+            steps += 1;
+
+            let instruction = instructions[instruction_index];
+            node.0 = match instruction {
+                'L' => network.get(node.0).unwrap().0,
+                'R' => network.get(node.0).unwrap().1,
+                _ => return Err(format!("Invalid character '{instruction}' in instructions")),
+            };
+
+            if node_loop.0 == 0 {
+                if let Some(start_of_loop) = node.1.get(&(node.0, instruction_index)) {
+                    println!("LOOP FOUND AT {} - {instruction_index}, length = {}", node.0, steps - start_of_loop);
+                    node_loop = (*start_of_loop, steps - start_of_loop, vec![]);
+                }
+                else {
+                    node.1.insert((node.0, instruction_index), steps);
+                }
+            }
+            else if node.0.ends_with("Z") {
+                node_loop.2.push(steps - (node_loop.0 + node_loop.1));
+            }
+
+            instruction_index = (instruction_index + 1) % instructions.len();
+
+            if node_loop.0 > 0 && steps - node_loop.0 > 2 * node_loop.1 {
+                break;
+            }
+        }
+
+        println!("{node_loop:?}");
+    }
+
     Ok(0)
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    let mut gcd = 1;
+    for i in 0..a.min(b) {
+        if a % i == 0 && b % i == 0 {
+            gcd = i;
+        }
+    }
+
+    a * b / gcd
 }
 
 #[cfg(test)]
@@ -70,9 +129,9 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let example: &str = include_str!("../../examples/day_8_1.txt");
+        let example: &str = include_str!("../../examples/day_8_3.txt");
         let parsed = parse(example).unwrap();
         let solution = part_two(&parsed);
-        assert_eq!(solution, Ok(0));
+        assert_eq!(solution, Ok(6));
     }
 }
