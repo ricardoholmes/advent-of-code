ce7fd1b6437af0bcd3a9c16d630f7798b45dabfd
ricardoholmes <ricardoholmes2004@gmail.com>
2023-12-08 16:35:05 +0000

    FINISHED DAY 8 FINALLY, SLOW BUT WORKSISHcargo run --release 8 -v

--- 2023/src/solutions/day_8.rs
+++ 2023/src/solutions/day_8.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 pub fn parse(input_raw: &str) -> Result<(Vec<char>, HashMap<&str, (&str, &str)>), String> {
     let mut lines = input_raw.lines();
@@ -44,22 +44,25 @@ pub fn part_one(input: &(Vec<char>, HashMap<&str, (&str, &str)>)) -> Result<u64,
     Ok(steps)
 }
 
-pub fn part_two(input: &(Vec<char>, HashMap<&str, (&str, &str)>)) -> Result<u64, String> {
+pub fn part_two(input: &(Vec<char>, HashMap<&str, (&str, &str)>)) -> Result<u128, String> {
     let (instructions, network) = input.clone();
     
-    let mut nodes: Vec<(&str, HashMap<(&str, usize), u64>)> = network
+    let mut nodes: Vec<(&str, HashMap<(&str, usize), u128>)> = network
         .keys()
         .filter(|key| key.ends_with("A"))
         .map(|node| (*node, HashMap::new()))
         .collect();
 
+    let mut loops = vec![];
     let mut instruction_index;
     for node in nodes.iter_mut() {
         instruction_index = 0;
         let mut steps = 0;
 
+        node.1.insert((node.0, instruction_index), steps);
+
         // (start_step_of_loop, length_of_loop, distance_to_ends)
-        let mut node_loop: (u64, u64, Vec<u64>) = (0, 0, vec![]);
+        let mut node_loop: (u128, u128, u128) = (0, 0, 0);
         loop {
             steps += 1;
 
@@ -70,35 +73,54 @@ pub fn part_two(input: &(Vec<char>, HashMap<&str, (&str, &str)>)) -> Result<u64,
                 _ => return Err(format!("Invalid character '{instruction}' in instructions")),
             };
 
-            if node_loop.0 == 0 {
+            if node_loop.1 == 0 {
                 if let Some(start_of_loop) = node.1.get(&(node.0, instruction_index)) {
-                    println!("LOOP FOUND AT {} - {instruction_index}, length = {}", node.0, steps - start_of_loop);
-                    node_loop = (*start_of_loop, steps - start_of_loop, vec![]);
+                    node_loop = (*start_of_loop, steps - start_of_loop, 0);
                 }
                 else {
                     node.1.insert((node.0, instruction_index), steps);
                 }
             }
             else if node.0.ends_with("Z") {
-                node_loop.2.push(steps - (node_loop.0 + node_loop.1));
+                node_loop.2 = steps - node_loop.1;
             }
 
             instruction_index = (instruction_index + 1) % instructions.len();
 
-            if node_loop.0 > 0 && steps - node_loop.0 > 2 * node_loop.1 {
+            if node_loop.1 > 0 && steps - node_loop.0 > 2 * node_loop.1 {
                 break;
             }
         }
 
-        println!("{node_loop:?}");
+        loops.push(node_loop);
     }
 
-    Ok(0)
+    let mut all_loop: (u128, u128, u128) = loops[0];
+    for (n, i) in loops.iter().enumerate() {
+        let i_length_big = i.1 * ((all_loop.2 / i.1) - 1);
+        let mut i_end = i.2 + i_length_big;
+        while all_loop.2 != i_end {
+            if i_end < all_loop.2 {
+                i_end += i.1;
+            }
+            else {
+                all_loop.2 += all_loop.1;
+            }
+        }
+        println!("{}", all_loop.2);
+
+        all_loop.1 = lcm(all_loop.1, i.1);
+        println!("{i:?} -> {all_loop:?} ({n} / {})", loops.len());
+    }
+
+    println!("{all_loop:?}");
+
+    Ok(all_loop.2)
 }
 
-fn lcm(a: u64, b: u64) -> u64 {
+fn lcm(a: u128, b: u128) -> u128 {
     let mut gcd = 1;
-    for i in 0..a.min(b) {
+    for i in 1..=a.min(b) {
         if a % i == 0 && b % i == 0 {
             gcd = i;
         }
