330f1fdbdabaf8838373dc2e25305f23ce4123a2
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-11 14:20:16 +0000

    Generalise expansion and counting into functions

--- 2023/src/solutions/day_11.rs
+++ 2023/src/solutions/day_11.rs
@@ -30,58 +30,21 @@ pub fn parse(input_raw: &str) -> Result<(Vec<Coord>, Vec<usize>, Vec<usize>), St
         }
     }
 
-    let mut empty_x: Vec<usize> = empty_x
+    let empty_x: Vec<usize> = empty_x
         .iter()
         .map(|x| *x)
         .collect();
-
-    empty_x.sort_unstable();
-    empty_x.reverse();
-
-    empty_y.reverse();
 
     Ok((stars, empty_x, empty_y))
 }
 
 pub fn part_one(input: &(Vec<Coord>, Vec<usize>, Vec<usize>)) -> Result<usize, String> {
     let (stars, empty_x, empty_y) = input;
+
     let mut stars = stars.clone();
-    expand(&mut stars, empty_x, empty_y, 2);
+    expand(&mut stars, empty_x.clone(), empty_y.clone(), 2)?;
 
-    let mut total_dist = 0;
-    let mut x_max = 0;
-    let mut y_max = 0;
-
-    for (i, star) in stars.iter().enumerate() {
-        if star.0 > x_max {
-            x_max = star.0;
-        }
-        if star.1 > y_max {
-            y_max = star.1;
-        }
-
-        for other_star in stars.iter().skip(i + 1) {
-            if star == other_star {
-                continue;
-            }
-            let total_dist_x = other_star.0 as i32 - star.0 as i32;
-            let total_dist_y = other_star.1 as i32 - star.1 as i32;
-
-            total_dist += total_dist_x.abs() as usize + total_dist_y.abs() as usize
-        }
-    }
-
-    // for y in 0..=y_max {
-    //     for x in 0..=x_max {
-    //         if stars.contains(&(x, y)) {
-    //             print!("{}", stars.iter().position(|&star| star == (x, y)).unwrap() + 1);
-    //         }
-    //         else {
-    //             print!(".");
-    //         }
-    //     }
-    //     println!();
-    // }
+    let total_dist = count_total_distances(&stars);
 
     Ok(total_dist)
 }
@@ -90,20 +53,17 @@ pub fn part_two(input: &(Vec<Coord>, Vec<usize>, Vec<usize>)) -> Result<usize, S
     let (stars, empty_x, empty_y) = input;
 
     let mut stars = stars.clone();
-    expand(&mut stars, empty_x, empty_y, 1_000_000);
+    expand(&mut stars, empty_x.clone(), empty_y.clone(), 1_000_000)?;
 
+    let total_dist = count_total_distances(&stars);
+
+    Ok(total_dist)
+}
+
+fn count_total_distances(stars: &[Coord]) -> usize {
     let mut total_dist = 0;
-    let mut x_max = 0;
-    let mut y_max = 0;
 
     for (i, star) in stars.iter().enumerate() {
-        if star.0 > x_max {
-            x_max = star.0;
-        }
-        if star.1 > y_max {
-            y_max = star.1;
-        }
-
         for other_star in stars.iter().skip(i + 1) {
             if star == other_star {
                 continue;
@@ -115,34 +75,32 @@ pub fn part_two(input: &(Vec<Coord>, Vec<usize>, Vec<usize>)) -> Result<usize, S
         }
     }
 
-    // for y in 0..=y_max {
-    //     for x in 0..=x_max {
-    //         if stars.contains(&(x, y)) {
-    //             print!("{}", stars.iter().position(|&star| star == (x, y)).unwrap() + 1);
-    //         }
-    //         else {
-    //             print!(".");
-    //         }
-    //     }
-    //     println!();
-    // }
-
-    Ok(total_dist)
+    total_dist
 }
 
-fn expand(stars: &mut Vec<Coord>, empty_x: &[usize], empty_y: &[usize], size_of_empty: usize) {
+fn expand(stars: &mut Vec<Coord>, mut empty_x: Vec<usize>, mut empty_y: Vec<usize>, size_of_empty: usize) -> Result<(), String> {
+    empty_x.sort_unstable();
+    empty_x.reverse();
+    empty_y.reverse();
+
+    if size_of_empty == 0 {
+        return Err(format!("Size of expanded empty lines must be >= 1."));
+    }
+
     for star in stars.iter_mut() {
-        for &x in empty_x {
+        for &x in &empty_x {
             if star.0 > x {
                 star.0 += size_of_empty - 1;
             }
         }
-        for &y in empty_y {
+        for &y in &empty_y {
             if star.1 > y {
                 star.1 += size_of_empty - 1;
             }
         }
     }
+
+    Ok(())
 }
 
 #[cfg(test)]
