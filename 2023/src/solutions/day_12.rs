27767ba6a11bd704853a4cf7e1b6bc43dec8a361
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-12 23:25:11 +0000

    Improve day 12 code

--- 2023/src/solutions/day_12.rs
+++ 2023/src/solutions/day_12.rs
@@ -1,4 +1,4 @@
-type Parsed = (Vec<usize>, Vec<usize>, Vec<usize>);
+type Parsed = (String, Vec<usize>);
 
 pub fn parse(input_raw: &str) -> Result<Vec<Parsed>, String> {
     let lines = input_raw.lines();
@@ -6,16 +6,10 @@ pub fn parse(input_raw: &str) -> Result<Vec<Parsed>, String> {
     let mut parsed_lines: Vec<Parsed> = vec![];
     for line in lines {
         let mut line_split = line.split_ascii_whitespace();
-        let line = line_split.next().unwrap().chars();
-        parsed_lines.push((
-            line.clone()
-                .enumerate()
-                .filter_map(|(i, c)| if c == '#' { Some(i) } else { None })
-                .collect(),
+        let pattern = line_split.next().unwrap().to_string();
 
-            line.enumerate()
-                .filter_map(|(i, c)| if c == '?' { Some(i) } else { None })
-                .collect(),
+        parsed_lines.push((
+            pattern,
 
             line_split
                 .next()
@@ -29,76 +23,104 @@ pub fn parse(input_raw: &str) -> Result<Vec<Parsed>, String> {
     Ok(parsed_lines)
 }
 
-pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
+pub fn part_one(input: &[Parsed]) -> Result<u64, String> {
     let mut total = 0;
-    for line in input {
-        let (known, unknown, groups) = line;
-        let unknown_permutations = get_permutations(unknown.len());
+    for (line_index, line) in input.iter().enumerate() {
+        let (pattern, groups) = line;
 
-        for permutation in unknown_permutations {
-            let mut springs = known.clone();
-            for i in permutation {
-                springs.push(unknown[i]);
-            }
-            springs.sort_unstable();
+        let pattern: Vec<char> = pattern.chars().collect();
 
-            if springs.len() == 0 {
-                if *groups == vec![0] {
-                    total += 1;
-                }
-                continue;
-            }
+        let start = total;
+        total += search(&pattern, &groups);
 
-            let mut group_sizes = vec![];
-
-            let mut previous = springs[0];
-            let mut current_group_size = 1;
-            for spring in &springs[1..] {
-                if previous == spring - 1 {
-                    current_group_size += 1;
-                }
-                else {
-                    group_sizes.push(current_group_size);
-                    current_group_size = 1;
-                }
-                previous = *spring;
-            }
-            group_sizes.push(current_group_size);
-
-            if group_sizes == *groups {
-                total += 1;
-            }
-        }
+        println!("{}/{} - {}", line_index + 1, input.len(), total - start);
     }
 
     Ok(total)
 }
 
-pub fn part_two(input: &[Parsed]) -> Result<usize, String> {
-    Ok(0)
+pub fn part_two(input: &[Parsed]) -> Result<u64, String> {
+    let mut total = 0;
+    for (line_index, line) in input.iter().enumerate() {
+        let (pattern, groups) = line;
+
+        let pattern: Vec<char> = pattern.repeat(5).chars().collect();
+        let groups: Vec<usize> = groups.repeat(5);
+
+        let start = total;
+        total += search(&pattern, &groups);
+
+        println!("{}/{} - {}", line_index + 1, input.len(), total - start);
+    }
+    Ok(total)
 }
 
-fn get_permutations(n: usize) -> Vec<Vec<usize>> {
-    if n == 0 {
-        return vec![vec![]];
+fn search(pattern: &[char], groups: &[usize]) -> u64 {
+    if pattern.is_empty() {
+        if groups.is_empty() {
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
 
-    let mut permutations: Vec<Vec<usize>> = vec![];
-    let perms = get_permutations(n - 1);
-    let perms_with_var: Vec<Vec<usize>> = perms
-        .clone()
-        .iter()
-        .map(|perm| {
-            let mut perm = perm.clone();
-            perm.push(n - 1);
-            perm
-        })
-        .collect();
+    let next_char = pattern[0];
+    if next_char == '.' {
+        let empty_end = pattern
+            .iter()
+            .position(|&c| c != '.')
+            .unwrap_or(pattern.len());
 
-    permutations.extend(perms);
-    permutations.extend(perms_with_var);
+        search(
+            pattern.get(empty_end..).unwrap_or_default(),
+            groups
+        )
+    }
+    else if next_char == '#' {
+        let spring_end = pattern
+            .iter()
+            .position(|&c| c != '#')
+            .unwrap_or(pattern.len());
 
-    permutations
+        match groups.get(0) {
+            Some(&group) => if spring_end > group {
+                0
+            } else {
+                let spring_end = match pattern.iter().skip(spring_end).position(|&c| c != '?') {
+                    Some(index) => index + spring_end,
+                    None => pattern.len(),
+                };
+
+                if spring_end >= group {
+                    search(
+                        pattern.get(group+1..).unwrap_or_default(),
+                        groups.get(1..).unwrap_or_default()
+                    )
+                } else if pattern.get(spring_end).is_some_and(|&c| c == '#') {
+                    search(
+                        &[
+                            &['#'].repeat(spring_end),
+                            pattern.get(spring_end..).unwrap_or_default()
+                        ].concat(),
+                        groups
+                    )
+                } else {
+                    0
+                }
+            },
+            None => 0,
+        }
+    }
+    else if next_char == '?' {
+        let rest_of_pattern = pattern.get(1..).unwrap_or_default();
+        let is_spring = [&['#'], rest_of_pattern].concat();
+
+        search(&is_spring, groups) + search(rest_of_pattern, groups)
+    }
+    else {
+        panic!("Invalid character in pattern!");
+    }
 }
 
 #[cfg(test)]
@@ -113,11 +135,11 @@ mod tests {
         assert_eq!(solution, Ok(21));
     }
 
-    #[test]
-    fn test_part2() {
-        let example = include_str!("../../examples/day_12_1.txt");
-        let parsed = parse(example).unwrap();
-        let solution = part_two(&parsed);
-        assert_eq!(solution, Ok(0));
-    }
+    // #[test]
+    // fn test_part2() {
+    //     let example = include_str!("../../examples/day_12_1.txt");
+    //     let parsed = parse(example).unwrap();
+    //     let solution = part_two(&parsed);
+    //     assert_eq!(solution, Ok(525152));
+    // }
 }
