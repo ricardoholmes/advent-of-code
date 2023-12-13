cbb66e795aac3a5d6b83817d4d46e3998946aec2
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-13 23:41:29 +0000

    Add day 13 part 2 solution + improved example from reddit

--- 2023/src/solutions/day_13.rs
+++ 2023/src/solutions/day_13.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 type Parsed = Vec<String>;
 
@@ -32,7 +32,7 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
             }
 
             for line_pos in vertical_lines.clone() {
-                let left = &line[..=line_pos].chars().rev().collect::<String>();
+                let left = line[..=line_pos].chars().rev().collect::<String>();
                 let left = left.as_str();
 
                 let right = &line[line_pos+1..];
@@ -43,11 +43,9 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
                 }
             }
         }
-        if vertical_lines.len() > 1 {
-            panic!("MULTIPLE REFLECTIONS?????");
-        }
-        else if vertical_lines.len() == 1 {
+        if vertical_lines.len() == 1 {
             total += vertical_lines.iter().next().unwrap() + 1;
+            continue;
         }
 
         let mut previous_lines = vec![];
@@ -67,7 +65,98 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
 }
 
 pub fn part_two(input: &[Parsed]) -> Result<usize, String> {
-    Ok(0)
+    let mut total = 0;
+    for pattern in input {
+        if let Some(n) = get_vertical_reflection(pattern) {
+            total += n;
+        }
+        
+        let transposed = transpose(pattern
+            .iter()
+            .map(|n| n.chars().collect())
+            .collect()
+        );
+
+        let pattern: Vec<String> = transposed
+            .iter()
+            .map(|line| line.iter().collect())
+            .collect();
+
+        if let Some(n) = get_vertical_reflection(&pattern) {
+            total += n * 100;
+            continue;
+        }
+    }
+
+    Ok(total)
+}
+
+fn get_vertical_reflection(pattern: &[String]) -> Option<usize> {
+    let mut vertical_lines: HashMap<usize, u8> = (0..(pattern[0].len() - 1))
+        .map(|n| (n, 0))
+        .collect();
+
+    for line in pattern {
+        if vertical_lines.len() == 0 {
+            break;
+        }
+
+        for &line_pos in vertical_lines.clone().keys() {
+            let left = line[..=line_pos].chars().rev().collect::<String>();
+            let right = &line[line_pos+1..];
+
+            let refl_len = left.len().min(right.len());
+            let left = &left[..refl_len];
+            let right = &right[..refl_len];
+            if left != right {
+                let smudges = *vertical_lines.get(&line_pos).unwrap();
+
+                if smudges > 0 {
+                    vertical_lines.remove(&line_pos);
+                    continue;
+                }
+
+                let mut differences = 0;
+                for i in 0..refl_len {
+                    if left[i..i+1] != right[i..i+1] {
+                        differences += 1;
+                    }
+                }
+
+                if differences != 1 {
+                    vertical_lines.remove(&line_pos);
+                    continue;
+                }
+
+                vertical_lines.insert(line_pos, smudges + 1);
+            }
+        }
+    }
+    vertical_lines = vertical_lines.iter().filter(|(_, &v)| v == 1).map(|(&k, &v)| (k, v)).collect();
+
+    if vertical_lines.len() != 1 {
+        None
+    }
+    else {
+        Some(*vertical_lines.keys().next().unwrap() as usize + 1)
+    }
+}
+
+fn transpose<T>(matrix: Vec<Vec<T>>) -> Vec<Vec<T>> where T: Clone {
+    let mut transposed: Vec<Vec<T>> = vec![];
+    for line in matrix {
+        for (i, c) in line.iter().enumerate() {
+            match transposed.get(i) {
+                Some(element) => {
+                    let mut transposed_line = element.clone();
+                    transposed_line.push(c.clone());
+                    transposed[i] = transposed_line;
+                }
+                None => transposed.push(vec![c.clone()]),
+            }
+        }
+    }
+    transposed
 }
 
 #[cfg(test)]
@@ -75,11 +164,18 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_transpose() {
+        let nums = vec![vec![1,2], vec![3,4]];
+        let goal = vec![vec![1,3], vec![2,4]];
+        assert_eq!(transpose(nums), goal);
+    }
+
+    #[test]
     fn test_part1() {
         let example = include_str!("../../examples/day_13_1.txt");
         let parsed = parse(example).unwrap();
         let solution = part_one(&parsed);
-        assert_eq!(solution, Ok(405));
+        assert_eq!(solution, Ok(709));
     }
 
     #[test]
@@ -87,6 +183,6 @@ mod tests {
         let example = include_str!("../../examples/day_13_1.txt");
         let parsed = parse(example).unwrap();
         let solution = part_two(&parsed);
-        assert_eq!(solution, Ok(0));
+        assert_eq!(solution, Ok(1400));
     }
 }
