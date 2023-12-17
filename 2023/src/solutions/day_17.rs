4d4b8deb99464c5117e6503981e545749c679a8e
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-17 14:56:05 +0000

    Add day 17 part 2 solution

--- 2023/src/solutions/day_17.rs
+++ 2023/src/solutions/day_17.rs
@@ -40,15 +40,15 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
     let mut crossed_vertical = 0;
     for i in 1..=3 {
         crossed_horizontal += input[0][i];
-        match movements.iter().position(|&(_, _, crossed, _)| crossed > crossed_horizontal) {
-            Some(index) => movements.insert(index, ((i, 0), Direction::Right, crossed_horizontal, HashSet::new())),
-            None => movements.push_back(((i, 0), Direction::Right, crossed_horizontal, HashSet::new())),
+        match movements.iter().position(|&(_, _, crossed)| crossed > crossed_horizontal) {
+            Some(index) => movements.insert(index, ((i, 0), Direction::Right, crossed_horizontal)),
+            None => movements.push_back(((i, 0), Direction::Right, crossed_horizontal)),
         }
 
         crossed_vertical += input[i][0];
-        match movements.iter().position(|&(_, _, crossed, _)| crossed > crossed_vertical) {
-            Some(index) => movements.insert(index, ((0, i), Direction::Down, crossed_vertical, HashSet::new())),
-            None => movements.push_back(((0, i), Direction::Down, crossed_vertical, HashSet::new())),
+        match movements.iter().position(|&(_, _, crossed)| crossed > crossed_vertical) {
+            Some(index) => movements.insert(index, ((0, i), Direction::Down, crossed_vertical)),
+            None => movements.push_back(((0, i), Direction::Down, crossed_vertical)),
         }
     }
 
@@ -58,21 +58,9 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
     let mut expanded = HashSet::new();
     while !movements.is_empty() {
         let movement = movements.pop_front().unwrap();
-        let (pos, direction, crossed, mut coords) = movement;
-        coords.insert(pos);
+        let (pos, direction, crossed) = movement;
 
         if pos == (border_x, border_y) {
-            // for y in 0..=border_y {
-            //     for x in 0..=border_x {
-            //         if coords.contains(&(x, y)) {
-            //             print!("#");
-            //         }
-            //         else {
-            //             print!(".");
-            //         }
-            //     }
-            //     println!();
-            // }
             return Ok(crossed);
         }
 
@@ -88,9 +76,9 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
                     let new_pos = (pos.0, pos.1 - distance);
                     let new_direction = Direction::Up;
                     first_dir_crossed += input[new_pos.1][new_pos.0];
-                    match movements.iter().position(|&(_, _, crossed, _)| crossed > first_dir_crossed) {
-                        Some(index) => movements.insert(index, (new_pos, new_direction, first_dir_crossed, coords.clone())),
-                        None => movements.push_back((new_pos, new_direction, first_dir_crossed, coords.clone())),
+                    match movements.iter().position(|&(_, _, crossed)| crossed > first_dir_crossed) {
+                        Some(index) => movements.insert(index, (new_pos, new_direction, first_dir_crossed)),
+                        None => movements.push_back((new_pos, new_direction, first_dir_crossed)),
                     }
                 }
 
@@ -98,9 +86,9 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
                     let new_pos = (pos.0, pos.1 + distance);
                     let new_direction = Direction::Down;
                     second_dir_crossed += input[new_pos.1][new_pos.0];
-                    match movements.iter().position(|&(_, _, crossed, _)| crossed > second_dir_crossed) {
-                        Some(index) => movements.insert(index, (new_pos, new_direction, second_dir_crossed, coords.clone())),
-                        None => movements.push_back((new_pos, new_direction, second_dir_crossed, coords.clone())),
+                    match movements.iter().position(|&(_, _, crossed)| crossed > second_dir_crossed) {
+                        Some(index) => movements.insert(index, (new_pos, new_direction, second_dir_crossed)),
+                        None => movements.push_back((new_pos, new_direction, second_dir_crossed)),
                     }
                 }
             }
@@ -109,9 +97,9 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
                     let new_pos = (pos.0 - distance, pos.1);
                     let new_direction = Direction::Left;
                     first_dir_crossed += input[new_pos.1][new_pos.0];
-                    match movements.iter().position(|&(_, _, crossed, _)| crossed > first_dir_crossed) {
-                        Some(index) => movements.insert(index, (new_pos, new_direction, first_dir_crossed, coords.clone())),
-                        None => movements.push_back((new_pos, new_direction, first_dir_crossed, coords.clone())),
+                    match movements.iter().position(|&(_, _, crossed)| crossed > first_dir_crossed) {
+                        Some(index) => movements.insert(index, (new_pos, new_direction, first_dir_crossed)),
+                        None => movements.push_back((new_pos, new_direction, first_dir_crossed)),
                     }
                 }
 
@@ -119,9 +107,9 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
                     let new_pos = (pos.0 + distance, pos.1);
                     let new_direction = Direction::Right;
                     second_dir_crossed += input[new_pos.1][new_pos.0];
-                    match movements.iter().position(|&(_, _, crossed, _)| crossed > second_dir_crossed) {
-                        Some(index) => movements.insert(index, (new_pos, new_direction, second_dir_crossed, coords.clone())),
-                        None => movements.push_back((new_pos, new_direction, second_dir_crossed, coords.clone())),
+                    match movements.iter().position(|&(_, _, crossed)| crossed > second_dir_crossed) {
+                        Some(index) => movements.insert(index, (new_pos, new_direction, second_dir_crossed)),
+                        None => movements.push_back((new_pos, new_direction, second_dir_crossed)),
                     }
                 }
             }
@@ -132,7 +120,120 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
 }
 
 pub fn part_two(input: &[Parsed]) -> Result<usize, String> {
-    Ok(0)
+    let border_x = input[0].len() - 1;
+    let border_y = input.len() - 1;
+
+    let mut movements = VecDeque::new();
+
+    let mut crossed_horizontal = 0;
+    let mut crossed_vertical = 0;
+    for i in 1..=10 {
+        if i <= border_x {
+            crossed_horizontal += input[0][i];
+            if i >= 4 {
+                match movements.iter().position(|&(_, _, crossed)| crossed > crossed_horizontal) {
+                    Some(index) => movements.insert(index, ((i, 0), Direction::Right, crossed_horizontal)),
+                    None => movements.push_back(((i, 0), Direction::Right, crossed_horizontal)),
+                }
+            }
+        }
+
+        if i <= border_y {
+            crossed_vertical += input[i][0];
+            if i >= 4 {
+                match movements.iter().position(|&(_, _, crossed)| crossed > crossed_vertical) {
+                    Some(index) => movements.insert(index, ((0, i), Direction::Down, crossed_vertical)),
+                    None => movements.push_back(((0, i), Direction::Down, crossed_vertical)),
+                }
+            }
+        }
+    }
+
+    let mut expanded = HashSet::new();
+    while !movements.is_empty() {
+        let movement = movements.pop_front().unwrap();
+        let (pos, direction, crossed) = movement;
+
+        if pos == (border_x, border_y) {
+            return Ok(crossed);
+        }
+
+        if !expanded.insert((pos, direction)) {
+            continue;
+        }
+
+        let mut first_dir_crossed = crossed;
+        let mut second_dir_crossed = crossed;
+        if direction.is_horizontal() {
+            for distance in 1..4 {
+                if distance <= pos.1 {
+                    first_dir_crossed += input[pos.1 - distance][pos.0];
+                }
+
+                if distance + pos.1 <= border_y {
+                    second_dir_crossed += input[pos.1 + distance][pos.0];
+                }
+            }
+        }
+        else {
+            for distance in 1..4 {
+                if distance <= pos.0 {
+                    first_dir_crossed += input[pos.1][pos.0 - distance];
+                }
+
+                if distance + pos.0 <= border_x {
+                    second_dir_crossed += input[pos.1][pos.0 + distance];
+                }
+            }
+        }
+
+        for distance in 4..=10 {
+            if direction.is_horizontal() {
+                if distance <= pos.1 {
+                    let new_pos = (pos.0, pos.1 - distance);
+                    let new_direction = Direction::Up;
+                    first_dir_crossed += input[new_pos.1][new_pos.0];
+                    match movements.iter().position(|&(_, _, crossed)| crossed > first_dir_crossed) {
+                        Some(index) => movements.insert(index, (new_pos, new_direction, first_dir_crossed)),
+                        None => movements.push_back((new_pos, new_direction, first_dir_crossed)),
+                    }
+                }
+
+                if distance + pos.1 <= border_y {
+                    let new_pos = (pos.0, pos.1 + distance);
+                    let new_direction = Direction::Down;
+                    second_dir_crossed += input[new_pos.1][new_pos.0];
+                    match movements.iter().position(|&(_, _, crossed)| crossed > second_dir_crossed) {
+                        Some(index) => movements.insert(index, (new_pos, new_direction, second_dir_crossed)),
+                        None => movements.push_back((new_pos, new_direction, second_dir_crossed)),
+                    }
+                }
+            }
+            else {
+                if distance <= pos.0 {
+                    let new_pos = (pos.0 - distance, pos.1);
+                    let new_direction = Direction::Left;
+                    first_dir_crossed += input[new_pos.1][new_pos.0];
+                    match movements.iter().position(|&(_, _, crossed)| crossed > first_dir_crossed) {
+                        Some(index) => movements.insert(index, (new_pos, new_direction, first_dir_crossed)),
+                        None => movements.push_back((new_pos, new_direction, first_dir_crossed)),
+                    }
+                }
+
+                if distance + pos.0 <= border_x {
+                    let new_pos = (pos.0 + distance, pos.1);
+                    let new_direction = Direction::Right;
+                    second_dir_crossed += input[new_pos.1][new_pos.0];
+                    match movements.iter().position(|&(_, _, crossed)| crossed > second_dir_crossed) {
+                        Some(index) => movements.insert(index, (new_pos, new_direction, second_dir_crossed)),
+                        None => movements.push_back((new_pos, new_direction, second_dir_crossed)),
+                    }
+                }
+            }
+        }
+    }
+
+    Err(format!("End not reached"))
 }
 
 #[cfg(test)]
@@ -148,10 +249,18 @@ mod tests {
     }
 
     #[test]
-    fn test_part2() {
+    fn test_part2_1() {
         let example = include_str!("../../examples/day_17_1.txt");
         let parsed = parse(example).unwrap();
         let solution = part_two(&parsed);
-        assert_eq!(solution, Ok(0));
+        assert_eq!(solution, Ok(94));
+    }
+
+    #[test]
+    fn test_part2_2() {
+        let example = include_str!("../../examples/day_17_2.txt");
+        let parsed = parse(example).unwrap();
+        let solution = part_two(&parsed);
+        assert_eq!(solution, Ok(71));
     }
 }
