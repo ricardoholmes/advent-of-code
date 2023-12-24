427b67b7b1661076e5dd4f8644031d9018699e15
Ricardo Holmes <ricardoholmes2004@gmail.com>
2023-12-24 13:32:09 +0100

    Tidy and add comments to day 24 part 1 solution

--- 2023/src/solutions/day_24.rs
+++ 2023/src/solutions/day_24.rs
@@ -123,42 +123,46 @@ pub fn part_two(input: &[Parsed]) -> Result<usize, String> {
 
 fn get_collisions_in_boundaries(hailstones: &[Parsed], min: f64, max: f64) -> Result<usize, String> {
     let mut collisions = 0;
-    for (index1, (s1, v1)) in hailstones.iter().enumerate() {
+    for (n, (s1, v1)) in hailstones.iter().enumerate() {
         let s1 = *s1;
         let Vector3 { x: x1, y: y1, z: _ } = s1;
         let v1 = *v1;
 
-        for (index2, (s2, v2)) in hailstones.iter().enumerate() {
-            if index2 <= index1 {
-                continue;
-            }
-
-            let s2 = *s2;
+        // skip to the (n+1)th hailstone so that
+        // - no hailstone is compared to itself
+        // - collisions for any two hailstones are only calculated once
+        //   - e.g. stone 1 will be checked for stone 0 BUT stone 0 will not be checked for stone 1
+        for &(s2, v2) in hailstones.iter().skip(n + 1) {
             let Vector3 { x: x2, y: y2, z: _ } = s2;
-            let v2 = *v2;
 
-
-            // if lines are parallel
+            // assuming no two lines are the same,
+            // if lines are parallel then they won't collide
             if v1.x * v2.y == v2.x * v1.y {
                 continue;
             }
 
+            // result of solving the simultaneous equations derived from s1 + (t1 * v1) = s2 + (t2 * v2)
             let t1 = (((x1 - x2) * v2.y) - ((y1 - y2) * v2.x)) / ((v1.y * v2.x) - (v1.x * v2.y));
             let t2 = ((y1 - y2) + (t1 * v1.y)) / v2.y;
 
+            // if collisions are in the past
             if t1 < 0.0 || t2 < 0.0 {
                 continue;
             }
 
+            // find collision
             let collision = s1 + (t1 * v1);
 
+            // if collision is out of bounds
             if collision.x < min || collision.x > max || collision.y < min || collision.y > max {
                 continue;
             }
 
             collisions += 1;
         }
+        panic!();
     }
+
     Ok(collisions)
 }
 
