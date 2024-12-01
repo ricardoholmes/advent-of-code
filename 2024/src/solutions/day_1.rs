2767249b6f17fe9bc711dfca1afa690164ff032d
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-12-01 10:52:02 +0000

    Complete day 1 part 2

--- 2024/src/solutions/day_1.rs
+++ 2024/src/solutions/day_1.rs
@@ -1,16 +1,17 @@
-pub fn parse(input_raw: &str) -> Result<&str, String> {
-    Ok(input_raw)
-}
-
-pub fn part_one(input_str: &&str) -> Result<u32, String> {
+pub fn parse(input_raw: &str) -> Result<(Vec<u32>, Vec<u32>), String> {
     let mut left: Vec<u32> = vec![];
     let mut right: Vec<u32> = vec![];
-    for line in input_str.lines() {
+    for line in input_raw.lines() {
         let line_split = line.split_ascii_whitespace().collect::<Vec<&str>>();
         left.push(line_split[0].parse().unwrap());
         right.push(line_split[1].parse().unwrap());
     }
 
+    Ok((left, right))
+}
+
+pub fn part_one(cols: &(Vec<u32>, Vec<u32>)) -> Result<u32, String> {
+    let (mut left, mut right) = cols.clone();
     left.sort();
     right.sort();
 
@@ -22,6 +23,11 @@ pub fn part_one(input_str: &&str) -> Result<u32, String> {
     Ok(out)
 }
 
-pub fn part_two(input_str: &&str) -> Result<u32, String> {
-    Ok(1)
+pub fn part_two(cols: &(Vec<u32>, Vec<u32>)) -> Result<u32, String> {
+    let (left, right) = cols.clone();
+    let mut total = 0;
+    for n in left {
+        total += n * right.iter().filter(|&&x| x == n).count() as u32
+    }
+    Ok(total)
 }
