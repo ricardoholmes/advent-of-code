e188e668dfefd8aa1ee9fc402994c9fedc65214c
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-12-01 10:46:09 +0000

    Complete 2024 day 1 part 1

--- 2024/src/solutions/day_1.rs
+++ 2024/src/solutions/day_1.rs
@@ -3,7 +3,23 @@ pub fn parse(input_raw: &str) -> Result<&str, String> {
 }
 
 pub fn part_one(input_str: &&str) -> Result<u32, String> {
-    Ok(1)
+    let mut left: Vec<u32> = vec![];
+    let mut right: Vec<u32> = vec![];
+    for line in input_str.lines() {
+        let line_split = line.split_ascii_whitespace().collect::<Vec<&str>>();
+        left.push(line_split[0].parse().unwrap());
+        right.push(line_split[1].parse().unwrap());
+    }
+
+    left.sort();
+    right.sort();
+
+    let mut out = 0;
+    for i in 0..left.len() {
+        out += left[i].abs_diff(right[i])
+    }
+
+    Ok(out)
 }
 
 pub fn part_two(input_str: &&str) -> Result<u32, String> {
