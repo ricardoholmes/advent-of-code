edd697e94d112631151ea5fb0ee20139bad53107
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-12-04 09:56:14 +0000

    Complete day 4 part 1

---
+++ 2024/src/solutions/day_4.rs
@@ -0,0 +1,94 @@
+type Parsed = String;
+
+pub fn parse(input_raw: &str) -> Result<Vec<Parsed>, String> {
+    Ok(input_raw.lines().map(|s| s.to_string()).collect())
+}
+
+pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
+    let mut count = 0;
+    for i in 0..input.len() {
+        for j in 0..input[i].len() {
+            /*
+                XMAS
+            */
+            if input[i].len() - j >= 4 && &input[i][j..j+4] == "XMAS" {
+                count += 1;
+            }
+            else if input[i].len() - j >= 4 && &input[i][j..j+4] == "SAMX" {
+                count += 1;
+            }
+
+            /*
+                X
+                M
+                A
+                S
+            */
+            if input.len() - i >= 4 {
+                if input[i].chars().nth(j).unwrap() == 'X' && input[i+1].chars().nth(j).unwrap() == 'M' && input[i+2].chars().nth(j).unwrap() == 'A' && input[i+3].chars().nth(j).unwrap() == 'S' {
+                    count += 1;
+                }
+                else if input[i].chars().nth(j).unwrap() == 'S' && input[i+1].chars().nth(j).unwrap() == 'A' && input[i+2].chars().nth(j).unwrap() == 'M' && input[i+3].chars().nth(j).unwrap() == 'X' {
+                    count += 1;
+                }
+            }
+
+            /*
+                X...
+                .M..
+                ..A.
+                ...S
+            */
+            if input.len() - i >= 4 && input[i].len() - j >= 4 {
+                if input[i].chars().nth(j).unwrap() == 'X' && input[i+1].chars().nth(j+1).unwrap() == 'M' && input[i+2].chars().nth(j+2).unwrap() == 'A' && input[i+3].chars().nth(j+3).unwrap() == 'S' {
+                    count += 1;
+                }
+                else if input[i].chars().nth(j).unwrap() == 'S' && input[i+1].chars().nth(j+1).unwrap() == 'A' && input[i+2].chars().nth(j+2).unwrap() == 'M' && input[i+3].chars().nth(j+3).unwrap() == 'X' {
+                    count += 1;
+                }
+            }
+
+            /*
+                ...S
+                ..A.
+                .M..
+                X...
+            */
+            if i >= 3 && input[i].len() - j >= 4 {
+                if input[i].chars().nth(j).unwrap() == 'X' && input[i-1].chars().nth(j+1).unwrap() == 'M' && input[i-2].chars().nth(j+2).unwrap() == 'A' && input[i-3].chars().nth(j+3).unwrap() == 'S' {
+                    count += 1;
+                }
+                else if input[i].chars().nth(j).unwrap() == 'S' && input[i-1].chars().nth(j+1).unwrap() == 'A' && input[i-2].chars().nth(j+2).unwrap() == 'M' && input[i-3].chars().nth(j+3).unwrap() == 'X' {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    Ok(count)
+}
+
+pub fn part_two(input: &[Parsed]) -> Result<usize, String> {
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let example = include_str!("../../examples/day_4_1.txt");
+        let parsed = parse(example).unwrap();
+        let solution = part_one(&parsed);
+        assert_eq!(solution, Ok(18));
+    }
+
+    #[test]
+    fn test_part2() {
+        let example = include_str!("../../examples/day_4_1.txt");
+        let parsed = parse(example).unwrap();
+        let solution = part_two(&parsed);
+        assert_eq!(solution, Ok(0));
+    }
+}
