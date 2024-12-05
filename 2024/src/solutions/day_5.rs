962d0f16b6c7ab62a2b3211eaa88aebe2dcdbd7e
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-12-05 14:30:22 +0000

    Complete day 5 part 2

--- 2024/src/solutions/day_5.rs
+++ 2024/src/solutions/day_5.rs
@@ -1,12 +1,12 @@
 use std::collections::{HashMap, HashSet};
 
-type Parsed = (HashMap<u32,HashSet<u32>>, Vec<Vec<u32>>);
+type Parsed = (HashMap<usize,HashSet<usize>>, Vec<Vec<usize>>);
 
 pub fn parse(input_raw: &str) -> Result<Parsed, String> {
     let mut input = input_raw.lines();
     let rules = input.by_ref().take_while(|x| !x.is_empty()).map(|x| {
         let (a, b) = x.split_once('|').unwrap();
-        (a.parse::<u32>().unwrap(), b.parse::<u32>().unwrap())
+        (a.parse::<usize>().unwrap(), b.parse::<usize>().unwrap())
     });
 
     let mut rules_dict = HashMap::new();
@@ -15,31 +15,17 @@ pub fn parse(input_raw: &str) -> Result<Parsed, String> {
     }
 
     let _ = input.by_ref().skip_while(|x| x.is_empty());
-    let pages = input.map(|x| x.split(',').map(|n| n.parse::<u32>().unwrap()).collect()).collect();
+    let pages = input.map(|x| x.split(',').map(|n| n.parse::<usize>().unwrap()).collect()).collect();
 
     Ok((rules_dict, pages))
 }
 
-pub fn part_one(input: &Parsed) -> Result<u32, String> {
+pub fn part_one(input: &Parsed) -> Result<usize, String> {
     let (rules, pages) = input.clone();
 
     let mut total = 0;
     for page in pages {
-        let mut valid = true;
-        let mut nums_past = HashSet::new();
-        for &num in &page {
-            if let Some(nums_must_be_after) = rules.get(&num) {
-                if let Some(_) = nums_past.intersection(nums_must_be_after).next() {
-                    valid = false;
-                }
-            }
-            if !valid {
-                break;
-            }
-            nums_past.insert(num);
-        }
-
-        if valid {
+        if is_valid(&rules, &page) {
             total += page[page.len() / 2]
         }
     }
@@ -48,7 +34,52 @@ pub fn part_one(input: &Parsed) -> Result<u32, String> {
 }
 
 pub fn part_two(input: &Parsed) -> Result<usize, String> {
-    Ok(0)
+    let (rules, pages) = input.clone();
+
+    let mut total = 0;
+    for page in pages {
+        if is_valid(&rules, &page) {
+            continue;
+        }
+
+        let mut page = page.clone();
+        let mut valid = false;
+        while !valid {
+            valid = true;
+            let mut nums_past = HashSet::new();
+            for i in 0..page.len() {
+                let num = page[i];
+                if let Some(nums_must_be_after) = rules.get(&num) {
+                    if let Some(&x) = nums_past.intersection(nums_must_be_after).next() {
+                        let j = page.iter().position(|&n| n == x).unwrap();
+                        page.swap(i, j);
+                        valid = false;
+                        break;
+                    }
+                }
+                nums_past.insert(num);
+            }
+        }
+
+        total += page[page.len() / 2];
+    }
+
+    Ok(total)
+}
+
+fn is_valid(rules: &HashMap<usize,HashSet<usize>>, page: &[usize]) -> bool {
+    let mut valid = true;
+    let mut nums_past = HashSet::new();
+    for &num in page {
+        if let Some(nums_must_be_after) = rules.get(&num) {
+            if let Some(_) = nums_past.intersection(nums_must_be_after).next() {
+                valid = false;
+                break;
+            }
+        }
+        nums_past.insert(num);
+    }
+    valid
 }
 
 #[cfg(test)]
@@ -68,6 +99,6 @@ mod tests {
         let example = include_str!("../../examples/day_5_1.txt");
         let parsed = parse(example).unwrap();
         let solution = part_two(&parsed);
-        assert_eq!(solution, Ok(0));
+        assert_eq!(solution, Ok(123));
     }
 }
