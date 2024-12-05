d7d93dcca99405d79a9fd49dc0eb0e1c8c8bce50
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-12-05 17:12:21 +0000

    Improve day 5 solution significantly by using built-in sorting

--- 2024/src/solutions/day_5.rs
+++ 2024/src/solutions/day_5.rs
@@ -1,23 +1,21 @@
-use std::collections::{HashMap, HashSet};
+use std::{cmp::Ordering, collections::HashSet};
 
-type Parsed = (HashMap<usize,HashSet<usize>>, Vec<Vec<usize>>);
+type Parsed = (HashSet<(usize,usize)>, Vec<Vec<usize>>);
 
 pub fn parse(input_raw: &str) -> Result<Parsed, String> {
     let mut input = input_raw.lines();
-    let rules = input.by_ref().take_while(|x| !x.is_empty()).map(|x| {
+    let rules: HashSet<(usize, usize)> = input.by_ref().take_while(|x| !x.is_empty()).map(|x| {
         let (a, b) = x.split_once('|').unwrap();
         (a.parse::<usize>().unwrap(), b.parse::<usize>().unwrap())
-    });
-
-    let mut rules_dict = HashMap::new();
-    for (k, v) in rules {
-        rules_dict.entry(k).or_insert(HashSet::new()).insert(v);
-    }
+    }).collect();
 
     let _ = input.by_ref().skip_while(|x| x.is_empty());
-    let pages = input.map(|x| x.split(',').map(|n| n.parse::<usize>().unwrap()).collect()).collect();
 
-    Ok((rules_dict, pages))
+    let pages = input
+            .map(|x| x.split(',').map(|n| n.parse::<usize>().unwrap()).collect())
+            .collect();
+
+    Ok((rules, pages))
 }
 
 pub fn part_one(input: &Parsed) -> Result<usize, String> {
@@ -25,8 +23,8 @@ pub fn part_one(input: &Parsed) -> Result<usize, String> {
 
     let mut total = 0;
     for page in pages {
-        if is_valid(&rules, &page) {
-            total += page[page.len() / 2]
+        if page.is_sorted_by(|&a, &b| rules.contains(&(a,b))) {
+            total += page[page.len() / 2];
         }
     }
 
@@ -37,48 +35,23 @@ pub fn part_two(input: &Parsed) -> Result<usize, String> {
     let (rules, pages) = input.clone();
 
     let mut total = 0;
-    for page in pages {
-        if is_valid(&rules, &page) {
+    for mut page in pages {
+        if page.is_sorted_by(|&a, &b| rules.contains(&(a,b))) {
             continue;
         }
 
-        let mut page = page.clone();
-        let mut valid = false;
-        while !valid {
-            valid = true;
-            let mut nums_past = HashSet::new();
-            for i in 0..page.len() {
-                let num = page[i];
-                if let Some(nums_must_be_after) = rules.get(&num) {
-                    if let Some(&x) = nums_past.intersection(nums_must_be_after).next() {
-                        let j = page.iter().position(|&n| n == x).unwrap();
-                        page.swap(i, j);
-                        valid = false;
-                    }
-                }
-                nums_past.insert(num);
+        page.sort_unstable_by(|&a, &b|
+            if rules.contains(&(a,b)) {
+                Ordering::Less
+            } else {
+                Ordering::Greater
             }
-        }
+        );
 
         total += page[page.len() / 2];
     }
 
     Ok(total)
-}
-
-fn is_valid(rules: &HashMap<usize,HashSet<usize>>, page: &[usize]) -> bool {
-    let mut valid = true;
-    let mut nums_past = HashSet::new();
-    for &num in page {
-        if let Some(nums_must_be_after) = rules.get(&num) {
-            if let Some(_) = nums_past.intersection(nums_must_be_after).next() {
-                valid = false;
-                break;
-            }
-        }
-        nums_past.insert(num);
-    }
-    valid
 }
 
 #[cfg(test)]
