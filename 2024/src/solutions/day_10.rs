f3bcb635484f6104280a5a1889769c6d05ab7177
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-12-10 12:46:56 +0000

    Complete day 10 part 1

---
+++ 2024/src/solutions/day_10.rs
@@ -0,0 +1,81 @@
+use std::collections::HashSet;
+
+type Parsed = Vec<u32>;
+
+pub fn parse(input_raw: &str) -> Result<Vec<Parsed>, String> {
+    Ok(
+        input_raw.lines()
+                 .map(|line| line.chars()
+                                       .map(|c| c.to_digit(10)
+                                                       .unwrap()
+                                            )
+                                       .collect()
+                    )
+                 .collect()
+    )
+}
+
+pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
+    let mut count = 0;
+    for (y, row) in input.iter().enumerate() {
+        for (x, &cell) in row.iter().enumerate() {
+            if cell != 0 {
+                continue;
+            }
+
+            let mut destinations = HashSet::new();
+            trailhead_score(input, &mut destinations, x, y);
+            count += destinations.len();
+        }
+    }
+    Ok(count)
+}
+
+fn trailhead_score(map: &[Parsed], destinations: &mut HashSet<(usize,usize)>, x: usize, y: usize) {
+    let cell = map[y][x];
+    if cell == 9 {
+        destinations.insert((x,y));
+        return;
+    }
+
+    if x > 0 && map[y][x-1] == cell + 1 {
+        trailhead_score(map, destinations, x-1, y);
+    }
+
+    if x + 1 < map[y].len() && map[y][x+1] == cell + 1 {
+        trailhead_score(map, destinations, x+1, y);
+    }
+
+    if y > 0 && map[y-1][x] == cell + 1 {
+        trailhead_score(map, destinations, x, y-1);
+    }
+
+    if y + 1 < map.len() && map[y+1][x] == cell + 1 {
+        trailhead_score(map, destinations, x, y+1);
+    }
+}
+
+pub fn part_two(input: &[Parsed]) -> Result<usize, String> {
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let example = include_str!("../../examples/day_10_1.txt");
+        let parsed = parse(example).unwrap();
+        let solution = part_one(&parsed);
+        assert_eq!(solution, Ok(36));
+    }
+
+    #[test]
+    fn test_part2() {
+        let example = include_str!("../../examples/day_10_1.txt");
+        let parsed = parse(example).unwrap();
+        let solution = part_two(&parsed);
+        assert_eq!(solution, Ok(0));
+    }
+}
--- 2024/src/solutions/mod.rs
+++ 2024/src/solutions/mod.rs
@@ -9,18 +9,20 @@ mod day_6;
 mod day_7;
 mod day_8;
 mod day_9;
+mod day_10;
 
 pub fn run(day: u8) -> Result<(String, String, TimesTaken), String> {
     match day {
-        1 => run_day!(day_1, 1),
-        2 => run_day!(day_2, 2),
-        3 => run_day!(day_3, 3),
-        4 => run_day!(day_4, 4),
-        5 => run_day!(day_5, 5),
-        6 => run_day!(day_6, 6),
-        7 => run_day!(day_7, 7),
-        8 => run_day!(day_8, 8),
-        9 => run_day!(day_9, 9),
-        _ => Err(format!("Invalid day number ({day})")),
+        1  => run_day!(day_1,   1),
+        2  => run_day!(day_2,   2),
+        3  => run_day!(day_3,   3),
+        4  => run_day!(day_4,   4),
+        5  => run_day!(day_5,   5),
+        6  => run_day!(day_6,   6),
+        7  => run_day!(day_7,   7),
+        8  => run_day!(day_8,   8),
+        9  => run_day!(day_9,   9),
+        10 => run_day!(day_10, 10),
+        _  => Err(format!("Invalid day number ({day})")),
     }
 }
