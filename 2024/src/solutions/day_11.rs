db807f05317cc7012cfccca4fee44da13c9b971f
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-12-11 13:06:04 +0000

    Complete day 11 part 2

--- 2024/src/solutions/day_11.rs
+++ 2024/src/solutions/day_11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 type Parsed = usize;
 
 pub fn parse(input_raw: &str) -> Result<Vec<Parsed>, String> {
@@ -6,21 +8,11 @@ pub fn parse(input_raw: &str) -> Result<Vec<Parsed>, String> {
     )
 }
 
-
-/*
-If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
-If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
-*/
-
-
 pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
     let mut stones = input.to_vec();
-    println!("Initial arrangement:");
-    println!("{}\n", stones.iter().map(|x| x.to_string() + " ").collect::<String>());
 
     // iterate for each blink
-    for i in 0..25 {
+    for _ in 0..25 {
         let mut new_stones = vec![];
         for stone in stones {
             // if engraved with 0, replaced by stone with 1
@@ -42,17 +34,43 @@ pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
             new_stones.push(stone * 2024);
         }
         stones = new_stones;
-
-        println!("{}: {}", i+1, stones.len());
-        // println!("After {} blinks:", i+1);
-        // println!("{}\n", stones.iter().map(|x| x.to_string() + " ").collect::<String>());
     }
 
     Ok(stones.len())
 }
 
 pub fn part_two(input: &[Parsed]) -> Result<usize, String> {
-    Ok(0)
+    let mut stones = HashMap::new();
+    for &i in input {
+        *stones.entry(i).or_default() += 1
+    }
+
+    // iterate for each blink
+    for _ in 0..75 {
+        let mut new_stones = HashMap::new();
+        for (stone, count) in stones {
+            // if engraved with 0, replaced by stone with 1
+            if stone == 0 {
+                *new_stones.entry(1).or_default() += count;
+                continue;
+            }
+
+            // if stone's length is even, split it in two
+            let n_len = stone.checked_ilog10().unwrap_or(0) + 1;
+            if n_len % 2 == 0 {
+                let factor_10 = 10_usize.pow(n_len / 2);
+                *new_stones.entry(stone / factor_10).or_default() += count;
+                *new_stones.entry(stone % factor_10).or_default() += count;
+                continue;
+            }
+
+            // otherwise, multiply stone by 2024
+            *new_stones.entry(stone * 2024).or_default() += count;
+        }
+        stones = new_stones;
+    }
+
+    Ok(stones.values().sum())
 }
 
 #[cfg(test)]
