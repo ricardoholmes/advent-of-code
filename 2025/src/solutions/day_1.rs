efb90d81066a70d2ff3c19896f11f3541247b00f
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-12-05 19:15:15 +0000

    Complete 2025 day 1

--- 2025/src/solutions/day_1.rs
+++ 2025/src/solutions/day_1.rs
@@ -1,13 +1,58 @@
-type Parsed = ();
+type Parsed = i32;
 
 pub fn parse(input_raw: &str) -> Result<Vec<Parsed>, String> {
-    Err(format!("Not implemented"))
+    let mut output = vec![];
+    for line in input_raw.lines() {
+        let n = line[1..].parse::<i32>().unwrap();
+        if line.starts_with('L') {
+            output.push(-n);
+        }
+        else {
+            output.push(n);
+        }
+    }
+
+    Ok(output)
 }
 
-pub fn part_one(input: &[Parsed]) -> Result<usize, String> {
-    Ok(0)
+pub fn part_one(input: &[Parsed]) -> Result<i32, String> {
+    let mut val = 50;
+    let mut count = 0;
+    for i in input {
+        val = (val + i + 100) % 100;
+        if val == 0 {
+            count += 1;
+        }
+    }
+
+    Ok(count)
 }
 
 pub fn part_two(input: &[Parsed]) -> Result<usize, String> {
-    Ok(0)
+    let mut val = 50;
+    let mut count = 0;
+    for &i in input {
+        let old_val = val;
+        val += i;
+        if val <= 0 {
+            if old_val == 0 && i != 0 {
+                val += 100
+            }
+            while val < 0 {
+                val += 100;
+                count += 1;
+            }
+            if val == 0 {
+                count += 1;
+            }
+        }
+        else if val >= 100 {
+            while val >= 100 {
+                val -= 100;
+                count += 1;
+            }
+        }
+    }
+
+    Ok(count)
 }
